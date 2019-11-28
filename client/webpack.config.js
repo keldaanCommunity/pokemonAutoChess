7a5aa33f051f0c696f72ded7199928c0d30a7c95
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2019-11-28 17:21:50 +0100

    more and more heroku stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'main.js'
+    },
     devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
