b0c0e46c14ab5b9596843a103ac5a247f2c218a6
arnaudgregoire <arnaud.gregoire@ensg.eu>
2020-06-21 14:29:47 +0200

    cost on refresh and level up

--- app/public/src/game/components/levelup-button.js
+++ app/public/src/game/components/levelup-button.js
@@ -6,7 +6,15 @@ export default class LevelUpButton extends Button {
   constructor(scene, x, y) {
     super(scene, x, y, 108, 108);
     this.background = new GameObjects.Image(scene, 0, 0, "levelUpButton");
+    this.textStyle = {
+      fontSize: "20px",
+      fontFamily: "Verdana",
+      color: "black",
+      align: "center"
+    };
     this.add(this.background);
+    this.add(new GameObjects.Text(scene, 60, -25, "4", this.textStyle));
+    this.add(new GameObjects.Image(scene, 90, -13, "money").setScale(0.5, 0.5));
   }
 
   enterButtonHoverState() {
--- app/public/src/game/components/refresh-button.js
+++ app/public/src/game/components/refresh-button.js
@@ -4,8 +4,16 @@ import Button from "./button";
 export default class RefreshButton extends Button {
   constructor(scene, x, y) {
     super(scene, x, y, 200, 50);
+    this.textStyle = {
+      fontSize: "20px",
+      fontFamily: "Verdana",
+      color: "black",
+      align: "center"
+    };
     this.background = new GameObjects.Image(scene, 0, 0, "refreshButton");
     this.add(this.background);
+    this.add(new GameObjects.Text(scene, 60, -25, "2", this.textStyle));
+    this.add(new GameObjects.Image(scene, 90, -13, "money").setScale(0.5, 0.5));
   }
 
   enterButtonHoverState() {
