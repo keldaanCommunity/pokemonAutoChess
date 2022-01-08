f6d97d1fa13b5a1104e2711341c3ff395b1af21f
keldaan <arnaudgregoire92@gmail.com>
2022-01-08 11:09:41 +0100

    shadow clone fix

--- app/public/src/game/components/pokemon-detail.js
+++ app/public/src/game/components/pokemon-detail.js
@@ -32,7 +32,7 @@ export default class PokemonDetail extends GameObjects.Container {
       wordWrap: {width: 400, useAdvancedWrap: true}
     };
 
-    let displayName = this.pokemonInformation.name.charAt(0).toUpperCase() + this.pokemonInformation.name.slice(1);
+    const displayName = this.pokemonInformation.name.charAt(0).toUpperCase() + this.pokemonInformation.name.slice(1);
 
     this.objType = 'detail';
     this.add(new GameObjects.Rectangle(scene, 360, 100, 720, 200, 0xffffff, 0.7).setStrokeStyle(3, 0x000000, 1));
