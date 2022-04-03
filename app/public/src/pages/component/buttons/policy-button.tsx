a1e6b114243668f43a34b2440adf9da23def8075
Keldaan <arnaudgregoire92@gmail.com>
2022-04-03 13:02:52 +0200

    buttons as tsx (#88)

---
+++ app/public/src/pages/component/buttons/policy-button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const buttonStyle = {
+    marginLeft:'10px',
+    marginTop:'10px',
+    marginRight:'10px'
+}
+
+export default function PolicyButton(){
+    function handlePrivacyPolicyClick(){
+        window.location.href = 'https://pokemonautochess-b18fb.web.app/privacy-policy.html';
+    }
+    return <button type="button" style={buttonStyle} className="nes-btn" onClick={()=>{handlePrivacyPolicyClick()}}>Privacy Policy</button>;
+}
