b2a3079118902216503a2c7d7237c97322fcafd8
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-04-04 16:50:26 +0200

    pmd collab

---
+++ edit/generate.ps1
@@ -0,0 +1,2 @@
+ts-node split.ts #path to \SpriteCollab
+TexturePacker.exe --verbose --multipack --pack-mode Good --sheet sheet/pokemons.png --data sheet/pokemons.json --texture-format png8 --format phaser ./split
