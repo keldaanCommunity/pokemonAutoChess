eb2bd3eea9f6cd7684768a4ad5b0102290ddfa96
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-04-05 16:21:26 +0200

    durations, credis

--- edit/generate.ps1
+++ edit/generate.ps1
@@ -1,2 +1,4 @@
-ts-node split.ts #path to \SpriteCollab
-TexturePacker.exe --verbose --multipack --pack-mode Good --sheet sheet/pokemons.png --data sheet/pokemons.json --texture-format png8 --format phaser ./split
+ts-node split.ts C:\Users\arnaud.gregoire\Desktop\pokemons\SpriteCollab
+foreach ($item in Get-ChildItem ./split) {
+    TexturePacker.exe --verbose --pack-mode Good --sheet sheets/${item}.png --data sheets/${item}.json --texture-format png8 --format phaser ./split/${item}
+}
