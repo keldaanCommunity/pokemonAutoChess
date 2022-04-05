0d8514b94287b91ea9d3f9042095a0284d95437d
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-04-05 17:12:03 +0200

    x y separation

--- edit/generate.ps1
+++ edit/generate.ps1
@@ -1,4 +1,4 @@
 ts-node split.ts C:\Users\arnaud.gregoire\Desktop\pokemons\SpriteCollab
 foreach ($item in Get-ChildItem ./split) {
-    TexturePacker.exe --verbose --pack-mode Good --sheet sheets/${item}.png --data sheets/${item}.json --texture-format png8 --format phaser ./split/${item}
+    TexturePacker.exe --verbose --pack-mode Good --sheet sheets/${item}.png --data sheets/${item}.json --texture-format png8 --format phaser  --trim-sprite-names ./split/${item}
 }
