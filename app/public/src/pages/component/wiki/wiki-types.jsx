3afb2e33d086b9ec799bb02e020db1a3ec8202ce
Chris Layco <toffjeoff20@gmail.com>
2022-04-15 17:00:49 -1000

    wiki synergy page fix

--- app/public/src/pages/component/wiki/wiki-types.jsx
+++ app/public/src/pages/component/wiki/wiki-types.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import WikiType from './wiki-type';
+import { Synergy } from '../../../../../types/enum/Synergy';
 
 class WikiTypes extends Component{
 
     render(){
         return <Tabs>
         <TabList>
-            {Object.keys(TYPE).map((r=>{
+            {Object.keys(Synergy).map((r=>{
+                console.log('synergies', r)
                 return <Tab key={'title-' + r}> <img src={"assets/types/" + r + ".png"}></img></Tab>
             }))}
         </TabList>
 
-        {Object.keys(TYPE).map((r=>{
+        {Object.keys(Synergy).map((r=>{
             return <TabPanel key={r}>
-                <WikiType type={r}/>
+                <WikiType type={Synergy[r]}/>
             </TabPanel>
         }))}
     </Tabs>
