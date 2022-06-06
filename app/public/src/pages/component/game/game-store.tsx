caa3f523cf26f4cf8df6d7d840e70dd7a89affe2
arnaudgregoire <arnaudgregoire92@gmail.com>
2022-06-06 10:16:43 +0200

    bacgroundcolor

--- app/public/src/pages/component/game/game-store.tsx
+++ app/public/src/pages/component/game/game-store.tsx
@@ -16,7 +16,7 @@ const style: CSS.Properties = {
 export default function GameStore() {
     const shop = useAppSelector(state=>state.game.shop)
     const pokemonCollection = useAppSelector(state=>state.game.pokemonCollection)
-    // console.log(pokemonCollection);
+    console.log(shop)
     return <ul style={style}>
     {shop.map((pokemon,index)=>{
         if(pokemon != Pkm.DEFAULT){
