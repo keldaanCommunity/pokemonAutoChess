1534ff55bf741078adc6cf092db7f27c4a24ffdf
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-07-05 14:14:08 +0200

    tapu

--- edit/generate.ps1
+++ edit/generate.ps1
@@ -1,4 +1,4 @@
-ts-node split.ts C:\Users\arnaud.gregoire\Desktop\SpriteCollab
+#ts-node split.ts C:\Users\arnaud.gregoire\Desktop\SpriteCollab
 foreach ($item in Get-ChildItem ./split) {
     TexturePacker.exe --verbose --pack-mode Good --sheet sheets/${item}.png --data sheets/${item}.json --texture-format png8 --format phaser  --trim-sprite-names ./split/${item}
 }
