6d5b50af81721f2eaa21a81239db669e066eaa84
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-10-03 11:04:30 +0200

    hide game items button

--- app/public/src/pages/component/game/game-items-proposition.tsx
+++ app/public/src/pages/component/game/game-items-proposition.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GameItem from './game-item'
 import CSS from 'csstype'
 import { useAppSelector } from '../../../hooks'
@@ -7,21 +7,29 @@ const style: CSS.Properties = {
     position:'absolute',
     top:'30%',
     left:'15.5%',
-    width:'60%',
-    display:'flex',
-    justifyContent:'space-between',
-    padding:'10px'
+    width:'60%'
 }
-
 
 export default function GameItemsProposition(){
     const itemsProposition = useAppSelector(state=>state.game.itemsProposition)
+    const [visible, setVisible] = useState(true)
     if(itemsProposition.length >0){
         return <div style={style}>
-            {itemsProposition.map((e,i)=>{
-                return <GameItem key={i} item={e}/>
-            })}
-    </div>
+            {visible ? <div style={{    
+                display:'flex',
+                justifyContent:'space-between',
+                padding:'10px'
+                }}>
+                {itemsProposition.map((e,i)=>{
+                    return <GameItem key={i} item={e}/>
+                })}
+            </div>: null}
+
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <button className='bubbly-warning' onClick={()=>{setVisible(!visible)}}>{visible ? 'Hide' : 'Show'}</button>
+            </div>
+        </div>
+
     }
     else{
         return null
