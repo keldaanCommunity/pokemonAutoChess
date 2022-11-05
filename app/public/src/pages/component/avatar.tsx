667cd7c4d6a88cc821555674b08a1bd0d70119c7
keldaan <arnaudgregoire92@gmail.com>
2022-11-05 08:22:35 +0100

    avatar fix

--- app/public/src/pages/component/avatar.tsx
+++ app/public/src/pages/component/avatar.tsx
@@ -9,10 +9,13 @@ export default function Avatar(props:{elo: number|undefined, name: string, avata
     return <div className='playerBox my-cursor' style={{display:'flex', justifyContent:'space-around', alignItems: 'center', flexFlow:'column'}}>
         <div style={{display:'flex', gap:'5px', justifyContent:'space-between', alignItems:'center'}}>
             <img style={{width:'40px',height:'40px'}} src={`${CDN_PORTRAIT_URL}${props.avatar}.png`}/>
-            <p style={{margin:'0px', color: '#ffc107'}}>{TitleName[props.title]}</p>
+            
             <p style={{margin:'0px', textAlign:'center'}}>{props.name?.length > 10 ?  props.name.slice(0,10).concat('..') : props.name}</p>
             <RoleBadge role={props.role}/>
         </div>
-        {elo}
+        <div style={{display:'flex', gap:'5px', alignItems:'center'}}>
+            <p style={{margin:'0px', color: '#ffc107'}}>{TitleName[props.title]}</p>
+            {elo}
+        </div>
     </div>
 }
