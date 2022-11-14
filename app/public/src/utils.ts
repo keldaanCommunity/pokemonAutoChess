4b39bd76f6452d9bb1ebcc7279580d128f6b4aa4
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2022-11-14 10:45:23 +0100

    fix shiny avatar for castform/megas

--- app/public/src/utils.ts
+++ app/public/src/utils.ts
@@ -1,14 +1,18 @@
 import { CDN_PORTRAIT_URL, Emotion } from "../../types"
 
-export function getPortraitSrc(index: string, shiny?: boolean, emotion?: Emotion){
-    const shinyPad = shiny ? "/0000/0001/" : ""
-    const emotionWithFallback = emotion ? emotion : Emotion.NORMAL
-    return `${CDN_PORTRAIT_URL}${index.replace(
-        "-",
-        "/"
-      )}${shinyPad}/${emotionWithFallback}.png`
+export function getPortraitSrc(
+  index: string,
+  shiny?: boolean,
+  emotion?: Emotion
+) {
+  const shinyPad = shiny ? (index.length === 4 ? "/0000/0001" : "/0001") : ""
+  const emotionWithFallback = emotion ? emotion : Emotion.NORMAL
+  return `${CDN_PORTRAIT_URL}${index.replace(
+    "-",
+    "/"
+  )}${shinyPad}/${emotionWithFallback}.png`
 }
 
-export function getAvatarSrc(avatar: string){
-    return `${CDN_PORTRAIT_URL}${avatar.replace('-','/')}.png`
+export function getAvatarSrc(avatar: string) {
+  return `${CDN_PORTRAIT_URL}${avatar.replace("-", "/")}.png`
 }
