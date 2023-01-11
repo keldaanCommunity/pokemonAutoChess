52c3b95abd3d9bd2eb708f93e947cb03ca6ff51b
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-01-11 01:29:36 +0100

    add comments for cache name

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'CACHE_V1';
+/* Change this cache name every time you want to force players 
+  to invalidate their cache and download all assets again */
+const CACHE_NAME = 'CACHE v2.4.0';
 
 // Cache-first strategy
 const cacheFirst = (event) => {
