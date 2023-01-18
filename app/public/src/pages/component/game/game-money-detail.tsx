87edadbe4e82ebb605f6d5d81035bec3e649f443
keldaan <arnaudgregoire92@gmail.com>
2023-01-18 09:35:28 +0100

    money-icon

--- app/public/src/pages/component/game/game-money-detail.tsx
+++ app/public/src/pages/component/game/game-money-detail.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useAppSelector } from "../../../hooks"
+import { Money } from "../icons/money"
 
 export default function GameMoneyDetail() {
   const streak = useAppSelector((state) => state.game.streak)
@@ -11,20 +12,14 @@ export default function GameMoneyDetail() {
           <p>Streak: </p>
           <div>
             {streak}
-            <img
-              style={{ width: "20px", height: "20px", marginBottom: "5px" }}
-              src="/assets/ui/money.png"
-            />
+            <Money />
           </div>
         </div>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <p>Interest: </p>
           <div>
             {interest}
-            <img
-              style={{ width: "20px", height: "20px", marginBottom: "5px" }}
-              src="/assets/ui/money.png"
-            />
+            <Money />
           </div>
         </div>
       </div>
