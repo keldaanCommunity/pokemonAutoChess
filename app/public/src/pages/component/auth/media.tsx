82d0304aa3e5ea5b26fcdb0ad384415d5b00eb00
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-01-19 02:20:36 +0100

    fix css media auth page

--- app/public/src/pages/component/auth/media.tsx
+++ app/public/src/pages/component/auth/media.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import DiscordButton from "../buttons/discord-button"
+import DonateButton from "../buttons/donate-button"
+import PolicyButton from "../buttons/policy-button"
 
 export default function Media() {
   return (
-    <div style={{ display: "flex", gap: "1em", alignItems: "center" }}>
+    <div style={{ display: "flex", gap: "1em", alignItems: "center", justifyContent: "end", marginRight: "1em" }}>
       <DiscordButton />
       <DonateButton />
       <PolicyButton />
