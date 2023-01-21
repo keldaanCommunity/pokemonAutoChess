b28f0cd821eff22ae1442cc79a8df0557277d1e3
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-01-21 13:19:28 +0100

    open/close options menu with escape key

--- app/public/src/pages/component/game/game-options-icon.tsx
+++ app/public/src/pages/component/game/game-options-icon.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import "./game-options-icon.css";
 import GameOptionsModal from "./game-options-modal";
 
 export default function GameOptionsIcon(props: { leave: () => void }){
     let [isOpen, setOpen] = useState<boolean>(false)
+
+    const handleKeyPress = useCallback((event) => {
+        if(event.key === "Escape"){
+            setOpen(!isOpen)
+        }
+      }, [isOpen]);
+    
+      useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+        return () => {
+          document.removeEventListener('keydown', handleKeyPress);
+        };
+      }, [handleKeyPress]);
+    
     return <>
     <div id="game-options-icon" className="nes-container clickable" onClick={() => { setOpen(true) }}>
         <img src="/assets/ui/options.svg" />
