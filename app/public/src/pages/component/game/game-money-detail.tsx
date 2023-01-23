ad9560d1b2b917f8e8372d22f5005304369453a0
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-01-23 21:46:01 +0100

    add more informations about gold income

--- app/public/src/pages/component/game/game-money-detail.tsx
+++ app/public/src/pages/component/game/game-money-detail.tsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { useAppSelector } from "../../../hooks"
 import { Money } from "../icons/money"
+import "./game-money-detail.css";
 
 export default function GameMoneyDetail() {
   const streak = useAppSelector((state) => state.game.streak)
   const interest = useAppSelector((state) => state.game.interest)
   return (
-    <div>
-      <p><Money value={`Streak: ${streak}`}/></p>
-      <p><Money value={`Interest: ${interest}`}/></p>
+    <div className="game-money-detail">
+      <p className="help">Each turn, gain 5 gold + 1 extra gold if you won the battle.</p>
+      <p><Money value={`Streak: +${streak}`}/></p>
+      <p className="help">Get 1 bonus gold for every victory or defeat streak, up to 5 bonus gold.</p>
+      <p><Money value={`Interest: +${interest}`}/></p>
+      <p className="help">Get 1 bonus gold per 10 gold saved, up to 5 bonus income at 50 saved gold.</p>
     </div>
   )
 }
