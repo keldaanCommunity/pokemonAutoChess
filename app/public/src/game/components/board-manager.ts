d390f294345d53673fea4ffe8572477c047b573c
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-02-06 18:19:08 +0100

    fix board not updated when switching player in battle

--- app/public/src/game/components/board-manager.ts
+++ app/public/src/game/components/board-manager.ts
@@ -100,10 +100,6 @@ export default class BoardManager {
   }
 
   setPlayer(player: IPlayer) {
-    if (this.mode == "battle") {
-      return
-    }
-
     if (player.id != this.player.id) {
       this.pokemons.forEach((pokemon, key) => {
         pokemon.destroy(true)
