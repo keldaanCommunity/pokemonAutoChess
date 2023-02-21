1650b1bd4843c43d07df56fbefc9c28a05bcc15a
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-02-21 09:35:55 +0100

    prevent hatch to evolve when 3 copied (#405)

--- app/public/src/pages/component/game/game-money-detail.tsx
+++ app/public/src/pages/component/game/game-money-detail.tsx
@@ -8,7 +8,6 @@ export default function GameMoneyDetail() {
   const currentPlayer = useAppSelector(state => state.game.players.find(p => p.id === state.game.currentPlayerId))  
   const lastPlayerBattle = currentPlayer && currentPlayer.history && currentPlayer.history.length > 0 ? currentPlayer.history.filter(r => !r.isPVE).at(-1) : null
   const lastBattleResult = lastPlayerBattle ? lastPlayerBattle.result : null
-  console.log({ lastBattleResult, lastPlayerBattle })
   const interest = useAppSelector((state) => state.game.interest)
   return (
     <div className="game-money-detail">
