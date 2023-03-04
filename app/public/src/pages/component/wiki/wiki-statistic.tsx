65ea864c7de967ff67c138c443c1ff3beb2299fb
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-03-04 14:34:43 +0100

    improve wiki stats tab (#441)

--- app/public/src/pages/component/wiki/wiki-statistic.tsx
+++ app/public/src/pages/component/wiki/wiki-statistic.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import CSS from "csstype"
+import { addIconsToDescription } from "../../utils/descriptions"
 
 const imgStyle: CSS.Properties = {
   width: "64px",
@@ -10,63 +11,61 @@ const imgStyle: CSS.Properties = {
 
 export default function WikiStatistic() {
   return (
-    <div style={{ display: "flex", justifyContent: "space-between", maxWidth: "960px" }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexFlow: "column"
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/hp.png"></img>
-          <p>Health points</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/def.png"></img>
-          <p>Defense points</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/speDef.png"></img>
-          <p>Special Defense points</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/mana.png"></img>
-          <p>Mana points</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/spellDamage.png"></img>
-          <p>Spell damage (%)</p>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexFlow: "column"
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/atk.png"></img>
-          <p>Attack damage</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/range.png"></img>
-          <p>Range, 1-range pokemon are melee.</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/speed.png"></img>
-          <p>Attack speed (Attack/second)</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/critChance.png"></img>
-          <p>Critical hit chance (%)</p>
-        </div>
-        <div style={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <img style={imgStyle} src="assets/icons/critDamage.png"></img>
-          <p>Critical hit damage (x multiplier)</p>
-        </div>
-      </div>
-    </div>
+    <ul className="wiki-stat">
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/hp.png"></img>
+        <p>Health points</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/shield.png"></img>
+        <p>Shield</p>
+        <p className="description">{addIconsToDescription(`Temporary health points. Cannot be healed.`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/def.png"></img>
+        <p>Defense points</p>
+        <p className="description">{addIconsToDescription(`Reduces PHYSICAL received`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/speDef.png"></img>
+        <p>Special Defense points</p>
+        <p className="description">{addIconsToDescription(`Reduces SPECIAL received`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/mana.png"></img>
+        <p>Mana points</p>
+        <p className="description">{addIconsToDescription(`Amount of mana required to cast ability. Pokemons receive mana with time (10 per second), when attacking (5 per attack) and when receiving damage (10% of the damage taken)`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/spellDamage.png"></img>
+        <p>Spell power (%)</p>
+        <p className="description">{addIconsToDescription(`Increase SPECIAL and ability effects in various ways.`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/atk.png"></img>
+        <p>Attack Damage</p>
+        <p className="description">{addIconsToDescription(`Damage inflicted on each basic attack`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/range.png"></img>
+        <p>Attack Range</p>
+        <p className="description">{addIconsToDescription(`1-range pokemon are melee`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/speed.png"></img>
+        <p>Attack Speed (Attack/second)</p>
+        <p className="description">{addIconsToDescription(`Number of attacks per second`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/critChance.png"></img>
+        <p>Critical hit chance (%)</p>
+        <p className="description">{addIconsToDescription(`Critical hits deal 2x more damage. Abilities cannot crit by default.`)}</p>
+      </li>
+      <li className="nes-container">
+        <img style={imgStyle} src="assets/icons/critDamage.png"></img>
+        <p>Critical hit damage</p>
+        <p className="description">{addIconsToDescription(`Multiplier for critical hits additional damage`)}</p>
+      </li>
+    </ul>
   )
 }
