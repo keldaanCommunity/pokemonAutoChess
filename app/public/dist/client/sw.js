34e9dba7a0b8e4f3c2bea255d04bd15c4d3e122f
arnaudgregoire <arnaudgregoire92@gmail.com>
2023-03-18 09:28:52 +0100

    bump cache

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = 'CACHE v2.9.0';
+const CACHE_NAME = 'CACHE v2.9.1';
 
 // Cache-first strategy
 const cacheFirst = (event) => {
