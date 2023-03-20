1e0b331ed484fc77d2db732fb194eda15d2c0f0b
keldaan <arnaudgregoire92@gmail.com>
2023-03-20 22:46:34 +0100

    update-item-script to run on prod when releasing the update

---
+++ db-commands/update-items.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv"
+import { connect } from "mongoose"
+import BotV2, { IBot } from "../app/models/mongo-models/bot-v2"
+import { Item } from "../app/types/enum/Item"
+
+async function main() {
+  dotenv.config()
+
+  try {
+    console.log("connect to db ...")
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const db = await connect(process.env.MONGO_URI!)
+    const bots = await BotV2.find()
+    for (let i = 0; i < bots.length; i++) {
+      let modified = false
+      const bot = bots[i]
+      bot.steps.forEach((step) => {
+        step.board.forEach((p) => {
+          for (let j = 0; j < p.items.length; j++) {
+            const item = p.items[j] as any
+            if (item === "KINGS_ROCK") {
+              console.log(p.name, item)
+              p.items[j] = Item.AMULET_COIN
+              modified = true
+            } else if (item === "WATER_INCENSE") {
+              console.log(p.name, item)
+              p.items[j] = Item.POWER_LENS
+              modified = true
+            } else if (item === "BRIGHT_POWDER") {
+              console.log(p.name, item)
+              p.items[j] = Item.STAR_DUST
+              modified = true
+            }
+          }
+        })
+      })
+      if (modified) {
+        bot.markModified("steps")
+        await bot.save()
+      }
+    }
+    await db.disconnect()
+  } catch (e) {
+    console.error("Parsing error:", e)
+  }
+}
+
+main()
