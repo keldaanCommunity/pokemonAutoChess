aa6f6d4521d86e56af6ecf1c2c1b46521fb8bd4a
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-04-23 16:05:55 +0200

    update esbuild phaser & minor deps

--- esbuild.js
+++ esbuild.js
@@ -1,16 +1,25 @@
-const { build } = require('esbuild')
+const { context } = require('esbuild')
 const isDev = process.argv[2] === "--dev"
 const isProdBuild = process.argv[2] === "--build"
 
-build({
+context({
   entryPoints: ["./app/public/src/index.tsx"],
   outfile: "app/public/dist/client/index.js",
   external: ["assets/*"],
   bundle: true,
   minify: isProdBuild,
   sourcemap: isProdBuild,
-  target: "es2016",  
-  watch: isDev
+  target: "es2016"
+}).then(context => {
+  if (isDev) {
+    // Enable watch mode
+    context.watch()
+  } else {
+    // Build once and exit if not in watch mode
+    context.rebuild().then(() => {
+      context.dispose()
+    })
+  }
 }).catch((error) => {
   console.error(error)
   process.exit(1)
