e68ce9f7aeefa74980cfb0cafb0cb2db60874834
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-04-27 17:28:07 +0200

    Tracer console: add error stacks (#555)
    * use console for tracer
    
    * change error formats
    
    * fix error format

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,1 +1,12 @@
-export const logger = require("tracer").colorConsole()
+export const logger = require("tracer").colorConsole({
+    format: [
+      '{{timestamp}} <{{title}}> {{message}} (in {{method}} - {{file}}:{{line}})', //default format
+      {
+        error:
+          '{{timestamp}} <{{title}}> {{message}} (in {{method}} - {{file}}:{{line}})\n{{stack}}' // error format
+      }
+    ],
+    preprocess: function(data) {
+        data.title = data.title.toUpperCase()
+    }
+})
