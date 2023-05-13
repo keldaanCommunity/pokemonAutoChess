4287211c34900345737d801d3e45c780292a45e3
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-05-13 17:30:56 +0200

    new tutorial video

--- app/public/src/pages/component/wiki/wiki-tutorials.tsx
+++ app/public/src/pages/component/wiki/wiki-tutorials.tsx
@@ -17,6 +17,10 @@ export default class WikiTutorials extends Component {
           <iframe width="560" height="315" src="https://www.youtube.com/embed/LCU5oSyZagw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           <h3>Game Screen explained</h3>
         </li>
+        <li className="nes-container">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/UjV2TkGYIuM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          <h3>5 Tips for Pokemon AutoChess</h3>
+        </li>
       </ul>
       <p>Thanks to <a href="https://www.youtube.com/@batotsu2751" target="_blank">Batotsu</a> for his work on tutorial videos !</p>
       </div>
