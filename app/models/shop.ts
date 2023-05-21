03ba29b2ad25e6b0d0515b8ec676a27211055356
elias-bina <71587299+elias-bina@users.noreply.github.com>
2023-05-21 10:20:53 +0200

    fix refill shop (on dev) (#641)
    * Allow to refill empty slots of the shop when locked (#633)
    
    * fix(ditto): Prevents ditto from taking a pokemon from the pool
    
    * Allow to refill empty slots of the shop when locked
    
    * fix(shop): Find empty shop slot
    
    * fix(ditto): Prevents ditto from eating the pool
    
    * fix(shop): fix typing of .map result

--- app/models/shop.ts
+++ app/models/shop.ts
@@ -96,17 +96,25 @@ export default class Shop {
     }
   }
 
+  refillShop(player: Player) {
+    // No need to release pokemons since they won't be changed
+    let PkmList = player.shop.map((pokemon) =>{
+      if(pokemon != Pkm.MAGIKARP && pokemon != Pkm.DEFAULT){
+        return pokemon
+      } 
+      return this.pickPokemon(player)
+    } )
+
+    for (let i = 0; i < 6; i++) {
+      player.shop[i] = PkmList[i]
+    }
+  }
+
   assignShop(player: Player) {
     player.shop.forEach((pkm) => this.releasePokemon(pkm))
 
     for (let i = 0; i < 6; i++) {
-      const seed = Math.random()
-      if (seed < DITTO_RATE) {
-        player.shop[i] = Pkm.DITTO
-      } else {
-        let pokemon = this.pickPokemon(player)
-        player.shop[i] = pokemon
-      }
+      player.shop[i] = this.pickPokemon(player)
     }
   }
 
@@ -182,10 +190,15 @@ export default class Shop {
 
   pickPokemon(player: Player) {
     const playerProbality = Probability[player.experienceManager.level]
-    const seed = Math.random()
+    const ditto_seed = Math.random()
+    const rarity_seed = Math.random()
     let pokemon = Pkm.MAGIKARP
     let threshold = 0
     const finals = new Array<Pkm>()
+    
+    if (ditto_seed < DITTO_RATE) {
+      return Pkm.DITTO
+    }
 
     player.board.forEach((pokemon: Pokemon) => {
       if (pokemon.final) {
@@ -195,7 +208,7 @@ export default class Shop {
 
     for (let i = 0; i < playerProbality.length; i++) {
       threshold += playerProbality[i]
-      if (seed < threshold) {
+      if (rarity_seed < threshold) {
         switch (i) {
           case 0:
             pokemon = this.getRandomPokemonFromPool(this.commonPool, finals)
@@ -214,13 +227,14 @@ export default class Shop {
             break
           default:
             logger.error(
-              `error in shop while picking seed = ${seed}, threshold = ${threshold}, index = ${i}`
+              `error in shop while picking seed = ${rarity_seed}, threshold = ${threshold}, index = ${i}`
             )
             break
         }
         break
       }
     }
+
     return pokemon
   }
 }
