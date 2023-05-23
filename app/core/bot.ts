91d171187019807d3fb26480df58ce6c59ae8dd2
elias-bina <elias.bina58@gmail.com>
2023-05-24 00:12:54 +0200

    fix(api): Add args in effects.update

--- app/core/bot.ts
+++ app/core/bot.ts
@@ -80,7 +80,7 @@ export default class Bot {
       }
 
       this.player.synergies.update(this.player.board)
-      this.player.effects.update(this.player.synergies)
+      this.player.effects.update(this.player.synergies, this.player.board)
     }
   }
 }
