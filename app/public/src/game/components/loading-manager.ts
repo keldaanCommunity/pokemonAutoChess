6bbfc495ac49b5f27366dfcec471a4cb014d270c
keldaan <arnaudgregoire92@gmail.com>
2023-06-06 19:05:13 +0200

    wynaut

--- app/public/src/game/components/loading-manager.ts
+++ app/public/src/game/components/loading-manager.ts
@@ -108,6 +108,11 @@ export default class LoadingManager {
       "/assets/attacks"
     )
     scene.load.multiatlas(
+      "COUNTER",
+      "/assets/attacks/COUNTER.json",
+      "/assets/attacks"
+    )
+    scene.load.multiatlas(
       "HEAL_ORDER",
       "/assets/attacks/HEAL_ORDER.json",
       "/assets/attacks"
