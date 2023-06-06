25f8c183d4b6108410e4c4708173305bcda8b769
keldaan <arnaudgregoire92@gmail.com>
2023-06-06 10:20:54 +0200

    remove flame orb

--- app/models/colyseus-models/player.ts
+++ app/models/colyseus-models/player.ts
@@ -39,9 +39,7 @@ export default class Player extends Schema implements IPlayer {
   @type("string") opponentAvatar: string = ""
   @type("string") opponentTitle: string = ""
   @type("uint8") boardSize: number = 0
-  @type({ collection: "string" }) items = new CollectionSchema<Item>([
-    Item.FLAME_ORB
-  ])
+  @type({ collection: "string" }) items = new CollectionSchema<Item>()
   @type("uint8") rank: number
   @type("uint16") elo: number
   @type("boolean") alive = true
