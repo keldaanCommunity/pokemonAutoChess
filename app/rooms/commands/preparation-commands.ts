2edacf02d804154bd72e2aa944d314361679e4e6
keldaan <arnaudgregoire92@gmail.com>
2023-06-07 08:23:04 +0200

    try a new metric with heapUsed && heapTotal

--- app/rooms/commands/preparation-commands.ts
+++ app/rooms/commands/preparation-commands.ts
@@ -13,6 +13,7 @@ import { pickRandomIn } from "../../utils/random"
 import { logger } from "../../utils/logger"
 import { entries, values } from "../../utils/schemas"
 import { MAX_PLAYERS_PER_LOBBY } from "../../types/Config"
+import { memoryUsage } from "node:process"
 
 export class OnJoinCommand extends Command<
   PreparationRoom,
@@ -118,16 +119,23 @@ export class OnGameStartRequestCommand extends Command<
           time: Date.now()
         })
       } else {
-        const freeMemory = os.freemem()
-        const totalMemory = os.totalmem()
+        let freeMemory = os.freemem()
+        let totalMemory = os.totalmem()
         logger.info(
-          `Memory: ${((100 * freeMemory) / totalMemory).toFixed(2)} % free (${
-            totalMemory - freeMemory
-          } / ${totalMemory})`
+          `Memory freemem/totalmem: ${(
+            (100 * freeMemory) /
+            totalMemory
+          ).toFixed(2)} % free (${totalMemory - freeMemory} / ${totalMemory})`
         )
-        if (process.env?.MODE === "dev") {
-          client.send(Transfer.GAME_START_REQUEST, "ok")
-        } else if (freeMemory < 0.1 * totalMemory) {
+        freeMemory = memoryUsage().heapUsed
+        totalMemory = memoryUsage().heapTotal
+        logger.info(
+          `Memory heapUsed/heapTotal: ${(
+            (100 * freeMemory) /
+            totalMemory
+          ).toFixed(2)} % free (${totalMemory - freeMemory} / ${totalMemory})`
+        )
+        if (freeMemory < 0.1 * totalMemory) {
           // if less than 10% free memory available, prevents starting another game to avoid out of memory crash
           this.room.broadcast(Transfer.MESSAGES, {
             name: "Server",
