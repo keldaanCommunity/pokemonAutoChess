4d84524196e0778b6bb1effed70abddeb6ea3ff9
keldaan <arnaudgregoire92@gmail.com>
2023-07-10 10:03:14 +0200

    error tracking on profanity-filter.ts

--- app/utils/profanity-filter.ts
+++ app/utils/profanity-filter.ts
@@ -1,16 +1,28 @@
-import blacklist from "./profanity-blacklist.json";
+import blacklist from "./profanity-blacklist.json"
 
-const blacklistRegexs = blacklist.map((word) => new RegExp(`\\b${removeDiactrics(word).replace(/(\W)/g, '\\$1')}\\b`, 'gi'))
-const splitRegex = /\b/ui; // NOTE: does not properly split strings with diacritics (éèà...)
+const blacklistRegexs = blacklist.map(
+  (word) =>
+    new RegExp(`\\b${removeDiactrics(word).replace(/(\W)/g, "\\$1")}\\b`, "gi")
+)
+const splitRegex = /\b/iu // NOTE: does not properly split strings with diacritics (éèà...)
 
-function removeDiactrics(str: string){
-    // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
-    return str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
+function removeDiactrics(str: string) {
+  // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+  return str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
 }
 
 export function cleanProfanity(string: string): string {
-    return string.split(splitRegex).map((word) => {
-        const normalizedWord = removeDiactrics(word);
-        return blacklistRegexs.some(regex => regex.test(normalizedWord)) ? "koffing" : word;
-    }).join(splitRegex.exec(string)![0]);
+  try {
+    return string
+      .split(splitRegex)
+      .map((word) => {
+        const normalizedWord = removeDiactrics(word)
+        return blacklistRegexs.some((regex) => regex.test(normalizedWord))
+          ? "koffing"
+          : word
+      })
+      .join(splitRegex.exec(string)![0])
+  } catch (error) {
+    return "error"
+  }
 }
