bbde192de981e0a5a3d35c78c07ff6babdce0f68
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-07-22 07:56:23 +0200

    Fix regression post merge canHoldItems (#830)
    * fix regression canHoldItems
    
    * revert config
    
    * simplify expression

--- app/models/colyseus-models/pokemon.ts
+++ app/models/colyseus-models/pokemon.ts
@@ -6,7 +6,7 @@ import { nanoid } from "nanoid"
 import { Emotion, IPokemon, AttackSprite } from "../../types"
 import { DEFAULT_ATK_SPEED, EvolutionTime } from "../../types/Config"
 import { Item } from "../../types/enum/Item"
-import { Pkm, PkmIndex } from "../../types/enum/Pokemon"
+import { Pkm, PkmIndex, Unowns } from "../../types/enum/Pokemon"
 import { Rarity, AttackType, PokemonActionState } from "../../types/enum/Game"
 import { Ability } from "../../types/enum/Ability"
 import { Synergy } from "../../types/enum/Synergy"
@@ -102,6 +102,10 @@ export class Pokemon extends Schema implements IPokemon {
       this.rarity !== Rarity.HATCH &&
       ![Pkm.DITTO, Pkm.EGG].includes(this.name)
     )
+  }
+
+  get canHoldItems(): boolean {
+    return ![Pkm.DITTO, Pkm.EGG, ...Unowns].includes(this.name)
   }
 
   get isOnBench(): boolean {
