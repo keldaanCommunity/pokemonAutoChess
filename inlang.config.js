c50c8c531f20de58ff4c2a9340352a904a837239
keldaan <arnaudgregoire92@gmail.com>
2023-07-25 11:48:47 +0200

    fix path

--- inlang.config.js
+++ inlang.config.js
@@ -1,7 +1,15 @@
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
 export async function defineConfig(env) {
 	const { default: i18nextPlugin } = await env.$import(
 		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
 	)
+
+	
+	const { default: sdkPlugin } = await env.$import(
+		'https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.9.0/dist/index.js'
+	);
 
     // recommended to enable linting feature
 	const { default: standardLintRules } = await env.$import(
@@ -12,7 +20,12 @@ export async function defineConfig(env) {
 		referenceLanguage: "en",
 		plugins: [
 			i18nextPlugin({
-				pathPattern: "app.public/dist/client/locales/{language}/translation.json",
+				pathPattern: "./app/public/dist/client/locales/{language}/translation.json",
+			}),
+			sdkPlugin({
+				languageNegotiation: {
+					strategies: [{ type: 'localStorage' }]
+				}
 			}),
             standardLintRules()
 		],
