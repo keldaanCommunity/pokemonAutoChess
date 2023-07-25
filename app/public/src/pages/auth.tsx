e0b91224c6cce2cce7ad06e29cbafb31e41e5e57
keldaan <arnaudgregoire92@gmail.com>
2023-07-25 13:24:53 +0200

    remove inlang sdk

--- app/public/src/pages/auth.tsx
+++ app/public/src/pages/auth.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import Login from "./component/auth/login"
 import Media from "./component/auth/media"
 import "./auth.css"
-import { i } from "@inlang/sdk-js"
+import { useTranslation } from "react-i18next"
 
 export default function Auth() {
+  const { t } = useTranslation()
+
   const isSupposedlyMobile =
     navigator.maxTouchPoints > 0 &&
     window.matchMedia("(orientation: portrait)").matches
@@ -12,12 +14,12 @@ export default function Auth() {
   return (
     <div className="auth-page">
       {isSupposedlyMobile && (
-        <p className="mobile-warning">{i("mobile_warning")}</p>
+        <p className="mobile-warning">{t("mobile_warning")}</p>
       )}
       <main>
         <h1>Pokemon Auto Chess</h1>
         <div className="nintendo">
-          <p>{i("nintendo_warning")}</p>
+          <p>{t("nintendo_warning")}</p>
         </div>
 
         <Login />
--- inlang.config.js
+++ inlang.config.js
@@ -1,33 +1,16 @@
 /**
- * @type { import("@inlang/core/config").DefineConfig }
- */
+* @type { import("@inlang/core/config").DefineConfig }
+*/
 export async function defineConfig(env) {
-	const { default: i18nextPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
-	)
+	const { default: i18nextPlugin } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js');
 
-	
-	const { default: sdkPlugin } = await env.$import(
-		'https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.9.0/dist/index.js'
-	);
-
-    // recommended to enable linting feature
-	const { default: standardLintRules } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
-	)
+	const { default: standardLintRules } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js');
 
 	return {
-		referenceLanguage: "en",
+		referenceLanguage: 'en',
 		plugins: [
-			i18nextPlugin({
-				pathPattern: "./app/public/dist/client/locales/{language}/translation.json",
-			}),
-			sdkPlugin({
-				languageNegotiation: {
-					strategies: [{ type: 'localStorage' }]
-				}
-			}),
-            standardLintRules()
+			i18nextPlugin({ pathPattern: 'app/public/dist/client/locales/{language}/translation.json' }),
+			standardLintRules(),
 		],
-	}
+	};
 }
