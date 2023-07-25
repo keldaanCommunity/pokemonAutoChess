a27090d16a4f0d682bc926f42b8aeef02d77383b
keldaan <arnaudgregoire92@gmail.com>
2023-07-25 10:04:06 +0200

    setup inlang.config.js

---
+++ inlang.config.js
@@ -0,0 +1,20 @@
+export async function defineConfig(env) {
+	const { default: i18nextPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
+	)
+
+    // recommended to enable linting feature
+	const { default: standardLintRules } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
+	)
+
+	return {
+		referenceLanguage: "en",
+		plugins: [
+			i18nextPlugin({
+				pathPattern: "./resources/{language}.json",
+			}),
+            standardLintRules()
+		],
+	}
+}
