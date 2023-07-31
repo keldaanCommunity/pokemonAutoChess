8fc31d06006f376ba4f81df2bcfd1eaa7742f988
keldaan <arnaudgregoire92@gmail.com>
2023-07-31 20:11:42 +0200

    buff victini
    # Conflicts:
    #	app/types/strings/Ability.ts

--- app/core/attack-strategy.ts
+++ app/core/attack-strategy.ts
@@ -4447,7 +4447,7 @@ export class SearingShotStrategy extends AttackStrategy {
     crit: boolean
   ) {
     super.process(pokemon, state, board, target, crit)
-    const damage = 20
+    const damage = 50
     const cells = board.getCellsInRadius(
       pokemon.positionX,
       pokemon.positionY,
