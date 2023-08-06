8524b2c8ab6eaa972b66aad449e0927e54f44405
DESKTOP-8SRFDDM\cam95 <cam9548@gmail.com>
2023-08-05 22:23:13 -0500

    show pokemon/item tooltip on hover instead of click

--- app/public/src/game/components/item-container.ts
+++ app/public/src/game/components/item-container.ts
@@ -65,47 +65,55 @@ export default class ItemContainer extends DraggableObject {
     this.add(this.detail)
 
     this.setInteractive()
-    this.input.dropZone = true
-    this.draggable = (this.pokemonId === null)
+    this.setDropZone(true)
+    this.draggable = this.pokemonId === null
   }
 
+  setDropZone(value: boolean) {
+    if (this.input) {
+      this.input.dropZone = value
+    }
+  }
 
   onPointerOver() {
-    //this.openDetail()
     super.onPointerOver()
-    this.input.dropZone = false
+    this.setDropZone(false)
+
     if (this.draggable) {
       this.circle?.setFillStyle(0x68829e)
+    }
+
+    if (!this.detail.visible) {
+      this.openDetail()
+      this.setDropZone(false)
     }
   }
 
   onPointerOut() {
     super.onPointerOut()
     if (!this.dragDisabled) {
-      this.input.dropZone = true
+      this.setDropZone(true)
     }
     if (this.draggable) {
       this.circle?.setFillStyle(0x61738a)
+    }
+    if (this.detail.visible) {
+      this.closeDetail()
+      this.setDropZone(true)
     }
   }
 
   onPointerDown(pointer: Phaser.Input.Pointer) {
     super.onPointerDown(pointer)
     this.parentContainer.bringToTop(this)
-    if (pointer.rightButtonDown()) {
-      if (!this.detail.visible) {
-        this.openDetail()
-        this.input.dropZone = false
-      } else {
-        this.closeDetail()
-        this.input.dropZone = true
-      }
+    if (this.detail.visible) {
+      this.closeDetail()
     }
   }
 
   onPointerUp() {
     super.onPointerUp()
-    this.input.dropZone = false
+    this.setDropZone(false)
   }
 
   openDetail() {
