16a37cfa2246320c214f8e82fc02cf6bc84fc8c4
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-08-15 17:01:18 +0200

    adjusted ultra color

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -108,7 +108,7 @@ export const RarityColor: { [key in Rarity]: string } = {
   [Rarity.UNCOMMON]: "#3bc95e",
   [Rarity.RARE]: "#41bfcc",
   [Rarity.EPIC]: "#ca6cee",
-  [Rarity.ULTRA]: "#E24916",
+  [Rarity.ULTRA]: "#E55B2D",
   [Rarity.UNIQUE]: "#ffffff",
   [Rarity.LEGENDARY]: "#e6cb49",
   [Rarity.MYTHICAL]: "#ffc0cb",
