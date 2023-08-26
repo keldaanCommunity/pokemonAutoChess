00261d78be074cd4931072e5a3be2e92c0e8e956
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-08-26 20:35:15 +0200

    Update profanity filter (#947)
    * fixed profanity filter adding whitespaces
    
    * add button title
    # Conflicts:
    #	app/public/src/pages/component/chat/chat-message.tsx

--- app/utils/profanity-filter.ts
+++ app/utils/profanity-filter.ts
@@ -1,27 +1,17 @@
 import blacklist from "./profanity-blacklist.json"
 
-const blacklistRegexs = blacklist.map(
-  (word) =>
-    new RegExp(`\\b${removeDiactrics(word).replace(/(\W)/g, "\\$1")}\\b`, "gi")
-)
-const splitRegex = /\b/iu // NOTE: does not properly split strings with diacritics (éèà...)
+// Word boundary that works with unicode, from: https://shiba1014.medium.com/regex-word-boundaries-with-unicode-207794f6e7ed
+const WORD_BOUNDARY_BEFORE = `(?<=[\\s,.:;!?"']|^)`
+const WORD_BOUNDARY_AFTER = `(?=[\\s,.:;!?"']|$)`
 
-function removeDiactrics(str: string) {
-  // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
-  return str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
-}
+const blacklistRegex = new RegExp(
+  `${WORD_BOUNDARY_BEFORE}(${blacklist.join("|")})${WORD_BOUNDARY_AFTER}`,
+  "gim"
+)
 
 export function cleanProfanity(string: string): string {
   try {
-    return string
-      .split(splitRegex)
-      .map((word) => {
-        const normalizedWord = removeDiactrics(word)
-        return blacklistRegexs.some((regex) => regex.test(normalizedWord))
-          ? "koffing"
-          : word
-      })
-      .join(" ")
+    return string.replace(blacklistRegex, "koffing")
   } catch (error) {
     console.error(error)
     return "error"
