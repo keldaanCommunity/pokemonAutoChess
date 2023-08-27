b32e636413a3559995dba74693609ffe524b462a
Cameron Pavao <cam9548@gmail.com>
2023-08-27 14:43:11 -0500

    redesign chat messages (#956)
    # Conflicts:
    #	app/public/src/pages/component/chat/chat-message.tsx

--- app/public/src/pages/component/chat/chat-message.tsx
+++ app/public/src/pages/component/chat/chat-message.tsx
@@ -3,63 +3,57 @@ import { useAppDispatch, useAppSelector } from "../../../hooks"
 import { removeMessage, searchById } from "../../../stores/NetworkStore"
 import { IChatV2, Role } from "../../../../../types"
 import { getAvatarSrc } from "../../../utils"
+import { cc } from "../../utils/jsx"
 
 export default function ChatMessage(props: { message: IChatV2 }) {
   const dispatch = useAppDispatch()
-  const role = useAppSelector((state) => state.lobby.user?.role)
-
-  const removeButton =
-    role && (role === Role.MODERATOR || role === Role.ADMIN) ? (
-      <button
-        className="bubbly red"
-        title="Remove message"
-        onClick={() => {
-          dispatch(
-            removeMessage({
-              id: props.message.id
-            })
-          )
-        }}
-      >
-        <p style={{ fontSize: "0.5em", margin: "0px" }}>X</p>
-      </button>
-    ) : null
+  const lobbyUser = useAppSelector((state) => state.lobby.user)
+  const preparationUser = useAppSelector((state) => state.preparation.user)
+  const user = lobbyUser ?? preparationUser
+  const role = user?.role
+  const time = formatDate(props.message.time)
 
   return (
-    <div
-      className="chat"
-      style={{
-        display: "flex",
-        flexFlow: "row nowrap",
-        alignItems: "top",
-        justifyContent: "start"
-      }}
-    >
-      <img
-        src={getAvatarSrc(props.message.avatar)}
-        style={{ alignSelf: "start" }}
-        className="pokemon-portrait"
-      />
-      <span
-        className="chat-message-author"
-        title={formatDate(props.message.time)}
-        onClick={() => {
-          dispatch(searchById(props.message.authorId))
-        }}
-      >
-        {props.message.author}
-      </span>
-      <p
-        style={{
-          fontSize: "1vw",
-          wordBreak: "break-word",
-          flex: "1",
-          margin: "0"
-        }}
-      >
-        {props.message.payload}
-      </p>
-      {removeButton}
+    <div className="chat">
+      <div className="chat-message-container">
+        {props.message.author && (
+          <div
+            className={cc("chat-user", {
+              sameUser: props.message.authorId === user?.id
+            })}
+          >
+            <img
+              src={getAvatarSrc(props.message.avatar)}
+              className="pokemon-portrait"
+            />
+            <div
+              className="author-and-time"
+              title="open profile"
+              onClick={() => dispatch(searchById(props.message.authorId))}
+            >
+              <span className="chat-message-author">
+                {props.message.author}
+              </span>
+              <span className="chat-message-time">{time}</span>
+            </div>
+            {role && (role === Role.MODERATOR || role === Role.ADMIN) && (
+              <button
+                className="remove-chat bubbly red"
+                onClick={() =>
+                  dispatch(
+                    removeMessage({
+                      id: props.message.id
+                    })
+                  )
+                }
+              >
+                <p style={{ fontSize: "0.5em", margin: "0px" }}>X</p>
+              </button>
+            )}
+          </div>
+        )}
+        <p className="chat-message">{props.message.payload}</p>
+      </div>
     </div>
   )
 }
