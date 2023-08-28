7eec812cee7dd22c9ad1b80aaca2384bb86110a9
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-08-28 14:24:30 +0200

    fix-lifebar-colors

--- app/public/src/game/components/life-bar.ts
+++ app/public/src/game/components/life-bar.ts
@@ -1,6 +1,9 @@
 import { Team } from "../../../../types/enum/Game"
 import Bar from "./bar"
 
+const ALLY_COLOR = "#76c442"
+const ENEMY_COLOR = "#e76e55"
+
 export default class LifeBar extends Bar {
   shieldProgress: HTMLDivElement
   constructor(
@@ -21,7 +24,7 @@ export default class LifeBar extends Bar {
       7,
       max,
       max,
-      team === 1 ? (flip ? "#76c442" : "#e76e55") : flip ? "#e76e55" : "#76c442"
+      team === (flip ? 1 : 0) ? ALLY_COLOR : ENEMY_COLOR
     )
 
     this.shieldProgress = document.createElement("div")
@@ -38,5 +41,12 @@ export default class LifeBar extends Bar {
   setShieldAmount(value: number) {
     this.shieldProgress.ariaValueNow = value.toString()
     this.shieldProgress.style.width = `${(value * 100) / this.max}%`
+  }
+
+  setTeam(team: number, flip: boolean) {
+    this.color = team === (flip ? 1 : 0) ? ALLY_COLOR : ENEMY_COLOR
+    if (this.progress) {
+      this.progress.style.backgroundColor = this.color
+    }
   }
 }
