c6f2ddcd48d3e77bb9eca879a122f75fa062cd6a
Keldaan <arnaudgregoire92@gmail.com>
2023-09-05 16:27:49 +0200

    new background image (#994)
    * new wall paper

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v4.0.32"
+const CACHE_NAME = "CACHE v4.0.33"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
