68b2c7eb1471200aabac5cbfbf0ebaf3c3fe281b
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-09-15 08:17:39 +0200

    add reconnect action (#1029)

--- app/public/src/pages/component/available-room-menu/game-room-item.tsx
+++ app/public/src/pages/component/available-room-menu/game-room-item.tsx
@@ -1,24 +1,32 @@
 import { RoomAvailable } from "colyseus.js"
 import React from "react"
 import { IGameMetadata } from "../../../../../types"
-import "./room-item.css"
 import { useTranslation } from "react-i18next"
+import { useAppSelector } from "../../../hooks"
+import { cc } from "../../utils/jsx"
+import "./room-item.css"
 
 export default function GameRoomItem(props: {
   room: RoomAvailable<IGameMetadata>
-  onSpectate: () => any
+  onJoin: (spectate: boolean) => void
 }) {
   const { t } = useTranslation()
+  const myUid = useAppSelector((state) => state.network.uid)
+  const spectate = !props.room.metadata?.playerIds.includes(myUid)
+
   return (
     <div className="room-item">
       <span className="room-name">{props.room.metadata?.name}</span>
       <span>
-        {props.room.clients} {t("player")}
-        {props.room.clients !== 1 ? "s" : ""}, {t("stage")}{" "}
+        {props.room.metadata?.playerIds.length} {t("player")}
+        {props.room.metadata?.playerIds.length !== 1 ? "s" : ""}, {t("stage")}{" "}
         {props.room.metadata?.stageLevel}
       </span>
-      <button className="bubbly blue" onClick={() => props.onSpectate()}>
-        {t("spectate")}
+      <button
+        className={cc("bubbly", spectate ? "blue" : "green")}
+        onClick={() => props.onJoin(spectate)}
+      >
+        {spectate ? t("spectate") : t("reconnect")}
       </button>
     </div>
   )
