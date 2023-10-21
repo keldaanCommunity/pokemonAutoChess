876b5719efc00b55971e4a4410805961e9e3aa7f
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-10-21 04:07:59 +0200

    precompute emotions and credits to no longer load tracker.json

--- app/public/src/pages/component/wiki/wiki-pokemon.tsx
+++ app/public/src/pages/component/wiki/wiki-pokemon.tsx
@@ -3,14 +3,11 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
 import PokemonFactory from "../../../../../models/pokemon-factory"
 import WikiPokemonDetail from "./wiki-pokemon-detail"
 import { PrecomputedRaritPokemonyAll } from "../../../../../types"
-import { ITracker } from "../../../../../types/ITracker"
 import PRECOMPUTED_RARITY_POKEMONS_ALL from "../../../../../models/precomputed/type-rarity-all.json"
-import tracker from "../../../../dist/client/assets/pokemons/tracker.json"
 import { Rarity } from "../../../../../types/enum/Game"
 import { Pkm, PkmIndex, PkmFamily } from "../../../../../types/enum/Pokemon"
 import { getPortraitSrc } from "../../../utils"
 
-const metadata = tracker as unknown as { [key: string]: ITracker }
 const precomputed =
   PRECOMPUTED_RARITY_POKEMONS_ALL as PrecomputedRaritPokemonyAll
 
@@ -40,17 +37,9 @@ export default function WikiPokemon(props: { rarity: Rarity }) {
       </TabList>
 
       {pokemons.map((pkm) => {
-        let m: ITracker | undefined = undefined
-        const pokemon = PokemonFactory.createPokemonFromName(pkm)
-        const pathIndex = pokemon.index.split("-")
-        if (pathIndex.length == 1) {
-          m = metadata[pokemon.index]
-        } else if (pathIndex.length == 2) {
-          m = metadata[pathIndex[0]].subgroups[pathIndex[1]]
-        }
         return (
           <TabPanel key={pkm}>
-            <WikiPokemonDetail pokemon={pkm} m={m} />
+            <WikiPokemonDetail pokemon={pkm} />
           </TabPanel>
         )
       })}
