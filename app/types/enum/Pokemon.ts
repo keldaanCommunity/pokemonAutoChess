4db6f847502bbbb819ee80a3dd6859b91ec15b86
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2023-11-20 17:39:43 +0100

    update anims

--- app/types/enum/Pokemon.ts
+++ app/types/enum/Pokemon.ts
@@ -2866,12 +2866,12 @@ export const AnimationConfig: {
   },
   [Pkm.MUNCHLAX]: {
     attack: AnimationType.Shoot,
-    ability: AnimationType.DeepBreath,
+    ability: AnimationType.Attack,
     emote: AnimationType.Shoot
   },
   [Pkm.SNORLAX]: {
-    attack: AnimationType.Stomp,
-    ability: AnimationType.Charge,
+    attack: AnimationType.Attack,
+    ability: AnimationType.Stomp,
     emote: AnimationType.Shoot
   },
   [Pkm.GROWLITHE]: {
