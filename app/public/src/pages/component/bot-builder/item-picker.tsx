cab6bd14e04e2a52f3bf8d890a1aa894fd665b09
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-01-01 18:06:10 +0100

    add tabs to item picker

--- app/public/src/pages/component/bot-builder/item-picker.tsx
+++ app/public/src/pages/component/bot-builder/item-picker.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react"
-import { Item } from "../../../../../types/enum/Item"
+import { ArtificialItems, BasicItems, Berries, CompletedItems, Item } from "../../../../../types/enum/Item"
 import { PkmWithConfig } from "../../../../../types"
 import { Tooltip } from "react-tooltip"
 import { ItemDetailTooltip } from "../../../game/components/item-detail"
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs"
+import { t } from "i18next"
+import ReactDOM from "react-dom"
 
 export default function ItemPicker(props: {
   selectEntity: React.Dispatch<React.SetStateAction<PkmWithConfig | Item>>
@@ -13,12 +16,28 @@ export default function ItemPicker(props: {
     e.dataTransfer.setData("item", item)
   }
 
+  const tabs = [
+    { label: t("components"), key: "components", items: BasicItems},
+    { label: t("completed_items"), key: "completed", items: CompletedItems},
+    { label: t("berries"), key: "berries", items: Berries},
+    { label: t("artificial_items"), key: "artificial_items", items: ArtificialItems },
+  ]
+
   return (
-    <div id="item-picker" className="nes-container">
-      {(Object.keys(Item) as Item[]).map((item) => (
+    <Tabs className="nes-container" id="item-picker">
+    <TabList>
+      {tabs.map((t) => (
+          <Tab key={t.key}>
+            {t.label}
+          </Tab>
+      ))}
+    </TabList>
+    {tabs.map((t) => (<TabPanel key={t.key}>
+      {t.items.map((item) => (
         <img
           key={item}
           src={"assets/item/" + Item[item] + ".png"}
+          className="item"
           data-tooltip-id="detail-item"
           onMouseOver={() => setItemHovered(item)}
           onClick={() => props.selectEntity(item)}
@@ -26,14 +45,15 @@ export default function ItemPicker(props: {
           onDragStart={(e) => handleOnDragStart(e, item)}
         />
       ))}
-      {itemHovered && (
+      </TabPanel>))}
+      {itemHovered && ReactDOM.createPortal(
         <Tooltip
           id="detail-item"
           className="custom-theme-tooltip item-detail-tooltip"
         >
           <ItemDetailTooltip item={itemHovered} />
         </Tooltip>
-      )}
-    </div>
+      , document.body)}
+    </Tabs>
   )
 }
