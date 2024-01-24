20df952f864e42774be9cbd518c55aa198018cdf
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-01-24 08:18:25 +0100

    Nightmare anim (#1425)
    * add anim to nightmare, fix cosmic power
    
    * fix a potential exception

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -2,7 +2,7 @@
   to invalidate their cache and download all assets again */
 
 
-const CACHE_NAME = "CACHE v4.7.0.18"
+const CACHE_NAME = "CACHE v4.7.0.19"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
