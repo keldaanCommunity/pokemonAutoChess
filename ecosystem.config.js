98951297e13a77aaeb70fb6c50490866eef6252c
Keldaan <keldaan.ag@gmail.com>
2024-04-10 17:56:42 +0200

    tryfix 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
         "user" : "deploy",  
         "host" : ["45.76.130.174"],  
         "ref"  : "origin/main",  
-        "repo" : "https://github.com/keldaanCommunity/pokemonAutoChess.git",  
+        "repo" : "git@github.com:keldaanCommunity/pokemonAutoChess.git",  
         "path" : "/home/deploy",  
         "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
       }  
