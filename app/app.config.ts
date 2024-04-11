e06209fa4a4097461cc337641eede77fdac267e9
Keldaan <keldaan.ag@gmail.com>
2024-04-11 11:07:13 +0200

    new rate limit configuration

--- app/app.config.ts
+++ app/app.config.ts
@@ -75,9 +75,10 @@ export default config({
     // set up rate limiter: maximum of five requests per minute
     const limiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-      legacyHeaders: false // Disable the `X-RateLimit-*` headers
+      max: 500, // Allow 500 requests per minute
+      message: "Too many requests, please try again later.",
+      statusCode: 429, // HTTP status code for rate limit exceeded
+      headers: true // Include custom headers
     })
 
     // Apply the rate limiting middleware to all requests
