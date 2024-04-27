038b19e723174e07f72b3a3d4980f289ee193208
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-04-28 01:46:40 +0200

    prepare poster for 5.0

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.0.0.162"
+const CACHE_NAME = "CACHE v5.0.0.163"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
