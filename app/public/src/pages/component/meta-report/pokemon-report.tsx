1ff69c4b44de3600c93f4555a03322e0bcfb74ff
Keldaan <keldaan.ag@gmail.com>
2024-05-20 17:46:53 +0200

    add pokemons-statistic-v2.ts (#1779)

--- app/public/src/pages/component/meta-report/pokemon-report.tsx
+++ app/public/src/pages/component/meta-report/pokemon-report.tsx
@@ -1,21 +1,24 @@
 import { t } from "i18next"
 import React, { useEffect, useMemo, useState } from "react"
+import { IPokemonsStatistic } from "../../../../../models/mongo-models/pokemons-statistic"
 import {
-  IPokemonsStatistic,
+  IPokemonsStatisticV2,
   fetchMetaPokemons
-} from "../../../../../models/mongo-models/pokemons-statistic"
+} from "../../../../../models/mongo-models/pokemons-statistic-v2"
+import { EloRank, EloRankThreshold } from "../../../../../types/Config"
 import { Rarity } from "../../../../../types/enum/Game"
 import { Synergy } from "../../../../../types/enum/Synergy"
-import PokemonStatistic from "./pokemon-statistic"
 import "./pokemon-report.css"
+import PokemonStatistic from "./pokemon-statistic"
 
 export function PokemonReport() {
   const [pokemonRankingBy, setPokemonRanking] = useState<string>("count")
   const [synergy, setSynergy] = useState<Synergy | "all">("all")
   const [rarity, setRarity] = useState<Rarity | "all">("all")
   const [loading, setLoading] = useState<boolean>(true)
+  const [eloThreshold, setEloTreshold] = useState<EloRank>(EloRank.BEGINNER)
 
-  const [metaPokemons, setMetaPokemons] = useState<IPokemonsStatistic[]>([])
+  const [metaPokemons, setMetaPokemons] = useState<IPokemonsStatisticV2[]>([])
   useEffect(() => {
     fetchMetaPokemons().then((res) => {
       setMetaPokemons(res)
@@ -24,15 +27,19 @@ export function PokemonReport() {
   }, [])
 
   const sortedMetaPokemons = useMemo(() => {
-    return [...metaPokemons].sort((a, b) => {
-      const order =
-        pokemonRankingBy === "count" || pokemonRankingBy === "item_count"
-          ? -1
-          : 1
-      return (a[pokemonRankingBy] - b[pokemonRankingBy]) * order
-    })
+    return [...metaPokemons].map((m) => ({
+      tier: m.tier,
+      pokemons: (Object.values(m.pokemons) || []).sort((a, b) => {
+        const order =
+          pokemonRankingBy === "count" || pokemonRankingBy === "item_count"
+            ? -1
+            : 1
+        return (a[pokemonRankingBy] - b[pokemonRankingBy]) * order
+      })
+    }))
   }, [metaPokemons, pokemonRankingBy])
 
+  console.log(sortedMetaPokemons)
   return (
     <div id="pokemon-report">
       <header>
@@ -79,11 +86,24 @@ export function PokemonReport() {
             </option>
           ))}
         </select>
+        <select
+          value={eloThreshold}
+          onChange={(e) => setEloTreshold(e.target.value as EloRank)}
+        >
+          {Object.keys(EloRank).map((r) => (
+            <option value={r} key={r}>
+              {t(`elorank.${r}`)} ({t("elo")} {">"} {EloRankThreshold[r]})
+            </option>
+          ))}
+        </select>
       </header>
       {loading && <p>{t("loading")}</p>}
       {
         <PokemonStatistic
-          pokemons={sortedMetaPokemons}
+          pokemons={
+            sortedMetaPokemons?.find((p) => p.tier === eloThreshold)
+              ?.pokemons || new Array<IPokemonsStatistic>()
+          }
           rankingBy={pokemonRankingBy}
           synergy={synergy}
           rarity={rarity}
