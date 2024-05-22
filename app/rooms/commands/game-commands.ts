fbaee9984ec0d966c622bdedf25f923afc512b17
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-05-22 13:34:50 +0200

    new title eternal

--- app/rooms/commands/game-commands.ts
+++ app/rooms/commands/game-commands.ts
@@ -827,6 +827,10 @@ export class OnUpdatePhaseCommand extends Command<GameRoom> {
       if (heal > 1000) {
         player.titles.add(Title.NURSE)
       }
+
+      if (this.state.stageLevel >= 40) {
+        player.titles.add(Title.ETERNAL)
+      }
     }
   }
 
