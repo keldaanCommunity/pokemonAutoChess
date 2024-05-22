62439a5855b514abad875f3928d7ddaf37fb1d25
Keldaan <keldaan.ag@gmail.com>
2024-05-22 13:37:34 +0200

    prod ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     apps: [{
         name        : "colyseus",
         script      : "./app/public/dist/server/app/index.js", // your entrypoint file
-        instances   : os.cpus().length,
+        instances   : 1,
         exec_mode   : "fork",         // IMPORTANT: do not use cluster mode.
         watch       : false,
         time        : true,
@@ -17,12 +17,21 @@ module.exports = {
     }],  
     deploy : {  
       production : {  
-        "user" : "deploy",  
-        "host" : ["45.76.130.174"],  
+        "user" : "root",  
+        "host" : ["146.190.113.96"],
+        "key"  : "./id_digital_ocean",
         "ref"  : "origin/prod",  
         "repo" : "git@github.com:keldaanCommunity/pokemonAutoChess.git",  
         "path" : "/home/deploy",  
         "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
-      }  
+      },
+      vultr : {  
+        "user" : "deploy",  
+        "host" : ["45.76.130.174"],
+        "ref"  : "origin/prod",  
+        "repo" : "git@github.com:keldaanCommunity/pokemonAutoChess.git",  
+        "path" : "/home/deploy",  
+        "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
+      }    
     }  
 }
