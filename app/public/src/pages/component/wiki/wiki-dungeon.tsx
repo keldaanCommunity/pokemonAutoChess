f707f8099a3175feba4b81bc0e539ac29befe5d5
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-05-27 13:37:34 +0200

    change family of burmy forms

--- app/public/src/pages/component/wiki/wiki-dungeon.tsx
+++ app/public/src/pages/component/wiki/wiki-dungeon.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next"
 import { PokemonClasses } from "../../../../../models/colyseus-models/pokemon"
 import { PRECOMPUTED_REGIONAL_MONS } from "../../../../../models/precomputed/precomputed-pokemon-data"
 import { DungeonDetails, DungeonPMDO } from "../../../../../types/enum/Dungeon"
-import { PkmFamily, PkmIndex } from "../../../../../types/enum/Pokemon"
+import { Pkm, PkmFamily, PkmIndex } from "../../../../../types/enum/Pokemon"
 import { getPortraitSrc } from "../../../utils"
 import SynergyIcon from "../icons/synergy-icon"
 
@@ -35,15 +35,22 @@ export function WikiDungeon() {
                   alt={dungeon}
                 />
                 <div className="wiki-dungeon-regional-mons">
-                    {PRECOMPUTED_REGIONAL_MONS
-                    .filter((p) => PokemonClasses[p].prototype.isInRegion(p, dungeon))
+                  {PRECOMPUTED_REGIONAL_MONS.filter((p) =>
+                    PokemonClasses[p].prototype.isInRegion(p, dungeon)
+                  )
                     .filter(
-                      (pkm, index, array) => array.map(p => PkmFamily[p]).indexOf(PkmFamily[pkm]) === index // dedup same family
-                    ).map(pkm => <img
-                      key={pkm}
-                      className="pokemon-portrait"
-                      src={getPortraitSrc(PkmIndex[pkm])}
-                    ></img>)}
+                      (pkm, index, array) =>
+                        array.findIndex(
+                          (p) => PkmFamily[p] === PkmFamily[pkm]
+                        ) === index // dedup same family
+                    )
+                    .map((pkm) => (
+                      <img
+                        key={pkm}
+                        className="pokemon-portrait"
+                        src={getPortraitSrc(PkmIndex[pkm])}
+                      ></img>
+                    ))}
                 </div>
               </li>
             )
