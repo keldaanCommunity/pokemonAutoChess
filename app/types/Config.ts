21c05ff2d444d3e52d35ff0c1fd9c25c515cd51f
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-06-29 14:08:55 +0200

    removed the 10am cest ultra bal ranked lobby

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -704,7 +704,7 @@ export const DTEF_TILESET_TILE_WIDTH = 24
 
 export const SCRIBBLE_LOBBY_CRON = "0 0 0-20/4 * * *" // every four hours from 00h to 20h
 export const GREATBALL_RANKED_LOBBY_CRON = "0 0 2,6,14,18 * * *" // every four hours from 2h to 22h
-export const ULTRABALL_RANKED_LOBBY_CRON = "0 0 10,22 * * *" // every four hours from 2h to 22h
+export const ULTRABALL_RANKED_LOBBY_CRON = "0 0 22 * * *" // every day 22h
 
 export const DUST_PER_BOOSTER = 50
 export const DUST_PER_SHINY = 250
