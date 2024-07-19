58b9ebc239c38249ec5cd8b076566358cc6cbb1e
Keldaan <keldaan.ag@gmail.com>
2024-07-19 09:08:58 +0200

    rebase

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.2.0.260"
+const CACHE_NAME = "CACHE v5.3.0.261"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
