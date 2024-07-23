75b3ec99db7d91e52ff768613e5c56da75f9c682
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-07-23 17:20:12 +0200

    badge without bootstrap

--- app/public/src/pages/component/profile/role-badge.tsx
+++ app/public/src/pages/component/profile/role-badge.tsx
@@ -2,22 +2,11 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 import { Role } from "../../../../../types"
 import { cc } from "../../utils/jsx"
-
-/*
-TODO
-port old classnames
-export const RoleColor: { [key in Role]: string } = {
-  [Role.ADMIN]: "success",
-  [Role.MODERATOR]: "primary",
-  [Role.BASIC]: "",
-  [Role.BOT]: "secondary",
-  [Role.BOT_MANAGER]: "danger"
-}
-*/
+import "./role-badge.css"
 
 export function RoleBadge(props: { role: Role }) {
   const { t } = useTranslation()
   return props.role && props.role !== Role.BASIC ? (
-    <div className={cc("badge", props.role)}>{t("role." + props.role)}</div>
+    <div className={cc("badge", props.role.toLowerCase())}>{t("role." + props.role)}</div>
   ) : null
 }
