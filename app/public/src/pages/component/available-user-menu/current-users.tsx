976d98b509b4515a8a3083818193bae3eabc66e2
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-07-31 18:20:00 +0200

    show total number of players in online count

--- app/public/src/pages/component/available-user-menu/current-users.tsx
+++ app/public/src/pages/component/available-user-menu/current-users.tsx
@@ -8,15 +8,18 @@ import "./current-users.css"
 
 export default function CurrentUsers() {
   const { t } = useTranslation()
-  const users: ILobbyUser[] = useAppSelector((state) => state.lobby.users)
+  const usersInLobby: ILobbyUser[] = useAppSelector((state) => state.lobby.users)
+  const nbUsersInPrepRoom = useAppSelector((state) => state.lobby.preparationRooms.reduce((total, r) => total + r.clients, 0))
+  const nbUsersInGameRoom = useAppSelector((state) => state.lobby.gameRooms.reduce((total, r) => total + r.clients, 0))
+  const nbTotalUsers = usersInLobby.length + nbUsersInPrepRoom + nbUsersInGameRoom
 
   return (
     <div className="my-container hidden-scrollable current-users-menu custom-bg">
       <h1>
-        {t("online")}: {users.length}
+        {t("online")}: {nbTotalUsers}
       </h1>
       <ul>
-        {users.map((v, i) => (
+        {usersInLobby.map((v, i) => (
           <User key={i} v={v} />
         ))}
       </ul>
