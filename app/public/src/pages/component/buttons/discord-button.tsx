d2e1e0986f3444d19b6718511fcf2e7fe206c19a
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-08-15 03:19:49 +0200

    add wiki to auth page

--- app/public/src/pages/component/buttons/discord-button.tsx
+++ app/public/src/pages/component/buttons/discord-button.tsx
@@ -6,7 +6,8 @@ export default function DiscordButton(props: { channel?: string }) {
     url = "https://discord.com/channels/737230355039387749/914503292875325461"
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
-      <button type="button" className="bubbly">
+      <button type="button" className="bubbly discord">
+        <img width={32} height={32} src={`assets/ui/discord.svg`} />
         Discord
       </button>
     </a>
