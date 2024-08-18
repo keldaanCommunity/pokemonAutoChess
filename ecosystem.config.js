0ec98ee5e44361e5db78bad6cfa31305384f01f5
Keldaan <keldaan.ag@gmail.com>
2024-08-18 17:30:25 +0200

    disable assetpack build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
         "ref"  : "origin/prod",  
         "repo" : "git@github.com:keldaanCommunity/pokemonAutoChess.git",  
         "path" : "/home/deploy",  
-        "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
+        "post-deploy" : "npm install && npm run build"  
       },
       vultr : {  
         "user" : "deploy",  
@@ -31,7 +31,7 @@ module.exports = {
         "ref"  : "origin/prod",  
         "repo" : "git@github.com:keldaanCommunity/pokemonAutoChess.git",  
         "path" : "/home/deploy",  
-        "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
+        "post-deploy" : "npm install && npm run build"  
       }    
     }  
 }
