87f76cffa67bc64b156e3ec10c6e82f765b0d0d7
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-08-19 20:45:27 +0200

    add timeout to loading manager (#2088)

--- app/public/src/game/components/loading-manager.ts
+++ app/public/src/game/components/loading-manager.ts
@@ -33,6 +33,7 @@ export default class LoadingManager {
 
   async preload() {
     const scene = this.scene
+    scene.load.xhr.timeout = 5000 // help avoiding failed loading of assets when server is overloaded
     scene.load.scenePlugin(
       "animatedTiles",
       AnimatedTiles,
