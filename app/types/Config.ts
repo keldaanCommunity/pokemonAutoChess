319ace5f0a86c7842f4f1e2cd83947eac55f607a
WhatAUsernameIHave <90142558+WhatAUsernameIHave@users.noreply.github.com>
2024-08-21 23:37:38 -0400

    Update Config.ts

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -39,7 +39,7 @@ export const RarityCost: { [key in Rarity]: number } = Object.freeze({
 })
 
 export const EmotionCost: { [key in Emotion]: number } = {
-  [Emotion.NORMAL]: -1,
+  [Emotion.NORMAL]: 0,
   [Emotion.HAPPY]: 0,
   [Emotion.PAIN]: 0,
   [Emotion.ANGRY]: 0,
