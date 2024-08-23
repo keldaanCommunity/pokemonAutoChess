6727ec810f2d939e7566501c9e18dbf6384eb821
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-08-23 17:58:40 +0200

    migrate pastebin-ts to pastedeno

--- app/services/pastebin.ts
+++ app/services/pastebin.ts
@@ -1,14 +1,13 @@
-import { FormatType } from "pastebin-ts"
-import { PastebinAPI } from "pastebin-ts/dist/api"
+import { FormatType, Pastebin, PrivacyLevel, ExpirationTime } from "pastedeno"
 
-let pastebinApi: PastebinAPI | undefined
+let pastebin: Pastebin | undefined
 
 if (
   process.env.PASTEBIN_API_DEV_KEY &&
   process.env.PASTEBIN_API_USERNAME &&
   process.env.PASTEBIN_API_DEV_KEY
 ) {
-  pastebinApi = new PastebinAPI({
+  pastebin = new Pastebin({
     api_dev_key: process.env.PASTEBIN_API_DEV_KEY!,
     api_user_name: process.env.PASTEBIN_API_USERNAME!,
     api_user_password: process.env.PASTEBIN_API_PASSWORD!
@@ -17,14 +16,16 @@ if (
 
 export const pastebinService = {
   createPaste(title: string, text: string, format: FormatType = "json") {
-    return pastebinApi?.createPaste({
+    return pastebin?.createPaste({
       text,
       title,
-      format
+      format,
+      privacy: PrivacyLevel.PRIVATE,
+      expiration: ExpirationTime.ONE_YEAR
     })
   },
 
   getPaste(id: string, raw: boolean) {
-    return pastebinApi?.getPaste(id, raw)
+    return pastebin?.getPaste(id, raw)
   }
 }
