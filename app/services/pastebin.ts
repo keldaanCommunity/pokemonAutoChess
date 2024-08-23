f4051aeea7f28236a42d3400acc17b898b63b1e9
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-08-23 18:29:30 +0200

    fix commonjs loading esm issue for pastedeno

--- app/services/pastebin.ts
+++ app/services/pastebin.ts
@@ -1,4 +1,4 @@
-import { FormatType, Pastebin, PrivacyLevel, ExpirationTime } from "pastedeno"
+import type { FormatType, Pastebin } from "pastedeno"
 
 let pastebin: Pastebin | undefined
 
@@ -7,21 +7,22 @@ if (
   process.env.PASTEBIN_API_USERNAME &&
   process.env.PASTEBIN_API_DEV_KEY
 ) {
-  pastebin = new Pastebin({
-    api_dev_key: process.env.PASTEBIN_API_DEV_KEY!,
-    api_user_name: process.env.PASTEBIN_API_USERNAME!,
-    api_user_password: process.env.PASTEBIN_API_PASSWORD!
-  })
+  ;(async () => {
+    const { Pastebin } = await import("pastedeno")
+    pastebin = new Pastebin({
+      api_dev_key: process.env.PASTEBIN_API_DEV_KEY!,
+      api_user_name: process.env.PASTEBIN_API_USERNAME!,
+      api_user_password: process.env.PASTEBIN_API_PASSWORD!
+    })
+  })()
 }
 
 export const pastebinService = {
-  createPaste(title: string, text: string, format: FormatType = "json") {
+  async createPaste(title: string, text: string, format: FormatType = "json") {
     return pastebin?.createPaste({
       text,
       title,
-      format,
-      privacy: PrivacyLevel.PRIVATE,
-      expiration: ExpirationTime.ONE_YEAR
+      format
     })
   },
 
