d9c96bd847c42cdf4f4212e688bea96d7238d469
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-08-23 18:56:36 +0200

    use modal component for bot select modal

--- app/public/src/pages/component/preparation/bot-select-modal.tsx
+++ app/public/src/pages/component/preparation/bot-select-modal.tsx
@@ -4,6 +4,7 @@ import { IBot } from "../../../../../models/mongo-models/bot-v2"
 import { useAppDispatch } from "../../../hooks"
 import { addBot } from "../../../stores/NetworkStore"
 import { cc } from "../../utils/jsx"
+import { Modal } from "../modal/modal"
 import { EloBadge } from "../profile/elo-badge"
 import { InlineAvatar } from "../profile/inline-avatar"
 import "./bot-select-modal.css"
@@ -47,9 +48,9 @@ export function BotSelectModal(props: { botsSelected: string[], close: () => voi
     )
 
   return (
-    <dialog open id="bot-select-modal" className="my-container">
-      <header>
-        <h2>{t("select_bots_for_this_game")}</h2>
+    <Modal show={true} className="bot-select-modal" onClose={() => props.close()}
+      header={<>
+        {t("select_bots_for_this_game")}
         <div className="spacer"></div>
         <input
           type="search"
@@ -74,33 +75,33 @@ export function BotSelectModal(props: { botsSelected: string[], close: () => voi
         >
           {t("sort_by_name")}
         </button>
-      </header>
-
-      {loading && <p>{t("loading")}</p>}
-      {!loading && botsListSorted.length === 0 && <p>{t("no_bots_found")}</p>}
-      <ul>
-        {botsListSorted.map((bot) => (
-          <li
-            className={cc("player", "my-box", "preparation-menu-user", {
-              selected: botsSelection.has(bot)
-            })}
-            onClick={() => {
-              if (botsSelection.has(bot)) {
-                botsSelection.delete(bot)
-              } else {
-                botsSelection.add(bot)
-              }
-              setBotsSelection(new Set([...botsSelection]))
-            }}
-            key={"proposition-bot-" + bot.id}
-          >
-            <EloBadge elo={bot.elo} />
-            <InlineAvatar avatar={bot.avatar} name={bot.name} />
-          </li>
-        ))}
-      </ul>
-
-      <footer className="actions">
+      </>}
+      body={<>
+        {loading && <p>{t("loading")}</p>}
+        {!loading && botsListSorted.length === 0 && <p>{t("no_bots_found")}</p>}
+        <ul>
+          {botsListSorted.map((bot) => (
+            <li
+              className={cc("player", "my-box", "preparation-menu-user", {
+                selected: botsSelection.has(bot)
+              })}
+              onClick={() => {
+                if (botsSelection.has(bot)) {
+                  botsSelection.delete(bot)
+                } else {
+                  botsSelection.add(bot)
+                }
+                setBotsSelection(new Set([...botsSelection]))
+              }}
+              key={"proposition-bot-" + bot.id}
+            >
+              <EloBadge elo={bot.elo} />
+              <InlineAvatar avatar={bot.avatar} name={bot.name} />
+            </li>
+          ))}
+        </ul>
+      </>}
+      footer={<>
         <button
           className="bubbly red"
           onClick={() => { props.close() }}
@@ -117,7 +118,7 @@ export function BotSelectModal(props: { botsSelected: string[], close: () => voi
           {t("add")} {botsSelection.size} {t("bot")}
           {botsSelection.size === 1 ? "" : "s"}
         </button>
-      </footer>
-    </dialog>
+      </>}
+    />
   )
 }
