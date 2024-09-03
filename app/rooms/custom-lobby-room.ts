8bb51d21d899c7bb994b402e3a5e144ebd6dbf77
ComplexRobot <complexrobotx@gmail.com>
2024-09-03 01:24:30 -0700

    Send user profile on reconnect (#2149)

--- app/rooms/custom-lobby-room.ts
+++ app/rooms/custom-lobby-room.ts
@@ -459,7 +459,12 @@ export default class CustomLobbyRoom extends Room<LobbyState> {
       if (consented) {
         throw new Error("consented leave")
       }
+      const userProfile = this.users.get(client.auth.uid)
+      if (!userProfile) {
+        throw new Error("Missing User Profile.")
+      }
       await this.allowReconnection(client, 30)
+      client.send(Transfer.USER_PROFILE, userProfile)
     } catch (error) {
       this.dispatcher.dispatch(new OnLeaveCommand(), { client })
     }
