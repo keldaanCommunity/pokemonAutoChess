b39b7dfa09742925bdfd03b58a40a8f24e8fc075
Keldaan <keldaan.ag@gmail.com>
2024-09-11 08:04:20 +0200

    release build files

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.4.0.301"
+const CACHE_NAME = "CACHE v5.5.0.302"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
