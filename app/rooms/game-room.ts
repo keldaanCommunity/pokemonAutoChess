c36547f836eb2bc24d4b361ee8bda63c22aadb8d
PAC-Defenestrator <ignacio.de.erausquin@gmail.com>
2024-09-16 00:42:37 -0500

    Porygon correctly drops rare candy on evo to porygon2 (#2211)

--- app/rooms/game-room.ts
+++ app/rooms/game-room.ts
@@ -900,16 +900,20 @@ export default class GameRoom extends Room<GameState> {
         )
         if (pokemonEvolved) {
           hasEvolved = true
-          // check item evolution rule after count evolution (example: Clefairy)
-          this.checkEvolutionsAfterItemAcquired(playerId, pokemonEvolved)
 
+          // Drop Rare Candy if no further count based evo
           if (
             pokemonEvolved.items.has(Item.RARE_CANDY) &&
-            pokemonEvolved.evolution === Pkm.DEFAULT
+            (pokemonEvolved.evolution === Pkm.DEFAULT ||
+              (pokemonEvolved.evolutionRule &&
+                !(pokemonEvolved.evolutionRule instanceof CountEvolutionRule)))
           ) {
             player.items.push(Item.RARE_CANDY)
             pokemonEvolved.items.delete(Item.RARE_CANDY)
           }
+
+          // check item evolution rule after count evolution (example: Porygon-2)
+          this.checkEvolutionsAfterItemAcquired(playerId, pokemonEvolved)
         }
       }
     })
