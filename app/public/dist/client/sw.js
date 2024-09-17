b60c4943eb4a8e3509d74ee254c338570ad2d442
Keldaan <keldaan.ag@gmail.com>
2024-09-17 21:54:42 +0200

    shadow lugia

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.5.0.306"
+const CACHE_NAME = "CACHE v5.5.0.307"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
