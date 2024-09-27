42675ee9f45cbb0cf31f56c932df44469704cc43
Keldaan <keldaan.ag@gmail.com>
2024-09-27 15:36:56 +0200

    download music from gh

--- app/public/src/pages/utils/audio.ts
+++ app/public/src/pages/utils/audio.ts
@@ -36,7 +36,7 @@ export function preloadSounds() {
 
 export function preloadMusic(scene: Scene, dungeonMusic: DungeonMusic) {
   scene.load.audio("music_" + dungeonMusic, [
-    `assets/musics/ogg/${dungeonMusic}.ogg`
+    `https://raw.githubusercontent.com/keldaanCommunity/pokemonAutoChessMusic/refs/heads/main/ogg/${dungeonMusic}.ogg`
   ])
 }
 
