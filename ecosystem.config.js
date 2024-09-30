03ced9a8c8623ef3d3252345b4832ffd21911791
Keldaan <keldaan.ag@gmail.com>
2024-09-30 07:49:51 +0200

    change max space size

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   apps: [{
     name: "colyseus",
     script: "./app/public/dist/server/app/index.js", // your entrypoint file
-    node_args: "--max-old-space-size=1024 --expose-gc",
+    node_args: "--max-old-space-size=1500 --expose-gc",
     instances: os.cpus().length,
     exec_mode: "fork",         // IMPORTANT: do not use cluster mode.
     watch: false,
@@ -19,7 +19,7 @@ module.exports = {
   deploy: {
     production: {
       "user": "root",
-      "host": ["164.92.98.9", "161.35.234.200", "143.198.101.153"],
+      "host": ["64.23.193.77", "164.92.98.9", "161.35.234.200", "143.198.101.153"],
       "ref": "origin/prod",
       "repo": "https://github.com/keldaanCommunity/pokemonAutoChess.git",
       "path": "/home/deploy",
