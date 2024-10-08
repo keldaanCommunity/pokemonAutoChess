e4b4b70bac9ee375eb8c00953a29e0db27eb4832
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-10-08 13:45:33 +0200

    fix egg chance reset on pve (#2299)

--- app/rooms/commands/game-commands.ts
+++ app/rooms/commands/game-commands.ts
@@ -1290,7 +1290,7 @@ export class OnUpdatePhaseCommand extends Command<GameRoom> {
           }
 
           player.eggChance = max(1)(player.eggChance + 0.25)
-        } else {
+        } else if(!isPVE){
           player.eggChance = 0
         }
 
