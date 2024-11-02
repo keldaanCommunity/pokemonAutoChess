b07c03f1667301d5e333c0a8bc4bad022347a54c
SuperRookie <hyunuk0417@gmail.com>
2024-11-02 17:33:21 +0900

    feat: Bot enable

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -766,6 +766,6 @@ export const CRON_ELO_DECAY_DELAY = 86400 * 1000 * 10 // 10 days
 export const CRON_ELO_DECAY_MINIMUM_ELO = 1100
 export const CRON_HISTORY_CLEANUP_DELAY = 86400 * 1000 * 30 // 30 days
 
-export const BOTS_ENABLED = false
+export const BOTS_ENABLED = true
 
 export { EloRank }
