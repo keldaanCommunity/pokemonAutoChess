4f7fde519879728daa73dacdecffd7ea8ccdedec
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-11-30 19:37:05 +0100

    reindex all sprites

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.7.0.398"
+const CACHE_NAME = "CACHE v5.7.0.399"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
