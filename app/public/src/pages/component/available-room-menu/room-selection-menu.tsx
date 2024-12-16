1d46cb8f0bc98990caa9a2676392e93639452b34
Fury_Phoenix <64714532+phoenix-starlight@users.noreply.github.com>
2024-12-16 10:00:35 -0800

    Prevent selecting/dragging button text

--- app/public/src/pages/component/available-room-menu/room-selection-menu.tsx
+++ app/public/src/pages/component/available-room-menu/room-selection-menu.tsx
@@ -13,22 +13,22 @@ export function RoomSelectionMenu(props: { show: boolean, onClose: () => void, o
         header={t("new_game")}
         body={<ul>
             <li className="my-box" onClick={() => props.onSelectMode(GameMode.QUICKPLAY)}>
-                <img src="assets/ui/game_modes/quickplay.png" alt="Quick Play" />
+                <img src="assets/ui/game_modes/quickplay.png" alt="Quick Play" draggable="false" />
                 <h2>{t("quick_play")}</h2>
                 <p>{t("quick_play_description")}</p>
             </li>
             <li className="my-box" onClick={() => props.onSelectMode(GameMode.RANKED)}>
-                <img src="assets/ui/game_modes/ranked.png" alt="Ranked match" />
+                <img src="assets/ui/game_modes/ranked.png" alt="Ranked match" draggable="false" />
                 <h2>{t("ranked_match")}</h2>
                 <p>{t("ranked_match_description")}</p>
             </li>
             <li className="my-box" onClick={() => props.onSelectMode(GameMode.SCRIBBLE)}>
-                <img src="assets/ui/game_modes/scribble.png" alt="Smeargle's Scribble" />
+                <img src="assets/ui/game_modes/scribble.png" alt="Smeargle's Scribble" draggable="false" />
                 <h2>{t("smeargle_scribble")}</h2>
                 <p>{t("smeargle_scribble_description")}</p>
             </li>
             <li className="my-box" onClick={() => props.onSelectMode(GameMode.CUSTOM_LOBBY)}>
-                <img src="assets/ui/game_modes/custom.png" alt="Custom lobby" />
+                <img src="assets/ui/game_modes/custom.png" alt="Custom lobby" draggable="false" />
                 <h2>{t("custom_room")}</h2>
                 <p>{t("custom_room_description")}</p>
             </li>
