c6ce28925a24077cf106552412fe3ee6ed5d6e6e
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-12-19 18:16:11 +0100

    adjust curse ability duration

--- app/core/abilities/abilities.ts
+++ app/core/abilities/abilities.ts
@@ -7608,7 +7608,8 @@ export class CurseStrategy extends AbilityStrategy {
     enemies.sort((a, b) => (a.status.curse ? +1 : b.hp - a.hp))
     const enemyWithHighestHP = enemies[0]
     const curseDelay =
-      ([8000, 4000][pokemon.stars - 1] ?? 4000) * (1 - (0.2 * pokemon.ap) / 100)
+      ([8000, 5000, 3000][pokemon.stars - 1] ?? 3000) *
+      (1 - (0.2 * pokemon.ap) / 100)
     enemyWithHighestHP.status.triggerCurse(curseDelay)
   }
 }
--- app/public/dist/client/changelog/patch-5.9.md
+++ app/public/dist/client/changelog/patch-5.9.md
@@ -49,6 +49,7 @@
 - Nerf Fiery Wrath (Galarian Moltres): damage 40 → 33
 - Nerf Mega Sableye: HP 230 → 200
 - Nerf Tsareena: PP 90 → 120
+- Adjusted Curse (Cursola): curse duration 8/4 → 8/5/3 seconds
 
 # Changes to Synergies
 
