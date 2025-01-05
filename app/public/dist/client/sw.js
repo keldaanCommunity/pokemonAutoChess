37911be515d24ed6de256f08fea0d18e25fb4a01
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-01-05 10:22:36 +0100

    add elgyem and beheeyem (#2640)
    * elgyem and beheeyem
    
    * change wonder room radius

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.8.0.434"
+const CACHE_NAME = "CACHE v5.8.0.437"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
