99993bef477eae2da8877b5a7916aab08c196598
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-01-06 07:44:31 +0100

    hotfix pokerus inanimate (#2653)
    Co-authored-by: Keldaan <keldaan.ag@gmail.com>

--- app/models/colyseus-models/status.ts
+++ app/models/colyseus-models/status.ts
@@ -1113,7 +1113,7 @@ export default class Status extends Schema implements IStatus {
   }
 
   triggerPokerus(pokemon: PokemonEntity) {
-    if ((pokemon.passive = Passive.INANIMATE)) return // Inanimate objects cannot get Pokerus
+    if (pokemon.passive === Passive.INANIMATE) return // Inanimate objects cannot get Pokerus
     if (!this.pokerus) {
       this.pokerus = true
     }
