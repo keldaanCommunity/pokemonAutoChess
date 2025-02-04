40a7a94bdf40f273135b1bd2269f178077281fe8
Keldaan <keldaan.ag@gmail.com>
2025-02-04 09:00:55 +0100

    bump to 5.10 && lts version for ecosystem.config.js

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v5.9.0.459"
+const CACHE_NAME = "CACHE v5.10.0.460"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
