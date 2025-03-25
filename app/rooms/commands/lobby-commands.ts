9857c93dd0186d1595abeca4825994c56becda26
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-03-25 08:28:30 +0100

    Store pending game id server-side (#2981)
    * store pending game id server side
    
    * remove debug console logs

--- app/rooms/commands/lobby-commands.ts
+++ app/rooms/commands/lobby-commands.ts
@@ -86,6 +86,13 @@ export class OnJoinCommand extends Command<
         // load existing account
         this.room.users.set(client.auth.uid, user)
         client.send(Transfer.USER_PROFILE, user)
+        const pendingGameId = await this.room.presence.hget(
+          client.auth.uid,
+          "pending_game_id"
+        )
+        if (pendingGameId != null) {
+          client.send(Transfer.RECONNECT_PROMPT, pendingGameId)
+        }
       } else {
         // create new user account
         const starterBoosters = 3
