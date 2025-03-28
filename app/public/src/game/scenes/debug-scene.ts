eca110c87aa2a6154405a0330b26ced8431cb338
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-03-28 10:00:03 +0100

    fixed debug scene map key

--- app/public/src/game/scenes/debug-scene.ts
+++ app/public/src/game/scenes/debug-scene.ts
@@ -168,14 +168,14 @@ export class DebugScene extends Phaser.Scene {
               "/assets/tilesets/" + mapName + "/" + t.image
             )
           })
-          this.load.tilemapTiledJSON("map", tilemap)
+          this.load.tilemapTiledJSON(mapName, tilemap)
           preloadMusic(this, DungeonDetails[mapName].music)
           this.load.once("complete", resolve)
           this.load.start()
         })
       })
       .then(() => {
-        const map = this.make.tilemap({ key: "map" })
+        const map = this.make.tilemap({ key: mapName })
         this.map = map
         this.tilemap!.layers.forEach((layer) => {
           const tileset = map.addTilesetImage(
