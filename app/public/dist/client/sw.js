1714a3f74374bc3866c8a2d79f2c328cb83b3c06
Zeki Xu <zekixu@gmail.com>
2025-04-25 11:10:38 -0700

    new board cell appearance (#3076)

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v6.1.0.538"
+const CACHE_NAME = "CACHE v6.1.0.550"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
