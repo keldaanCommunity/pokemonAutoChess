70a4897069b94fe53a3ae35aab9689838a02e00b
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-05-04 21:56:13 +0200

    assetpack

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v6.1.0.554"
+const CACHE_NAME = "CACHE v6.1.0.555"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
