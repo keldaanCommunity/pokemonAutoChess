6afa7bc14f58448c89dd74d1e16246bd02ebf3d6
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-03 23:33:29 +0200

    revert LINK_CABLE animation change

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v6.2.0.579"
+const CACHE_NAME = "CACHE v6.2.0.580"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
