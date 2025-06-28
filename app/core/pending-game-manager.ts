6171c13054f4a186e60728875470f8e9e0517a87
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-29 01:20:08 +0200

    fix typescript errors

--- app/core/pending-game-manager.ts
+++ app/core/pending-game-manager.ts
@@ -1,29 +1,35 @@
-import { type Presence } from "@colyseus/core";
+import { type Presence } from "@colyseus/core"
+import { ALLOWED_GAME_RECONNECTION_TIME } from "../types/Config"
+import { isValidDate } from "../utils/date"
 import { logger } from "../utils/logger"
-import { isValidDate } from "../utils/date";
-import { ALLOWED_GAME_RECONNECTION_TIME } from "../types/Config";
 
 interface PendingGame {
-    gameId: string;
-    reconnectionDeadline: Date;
-    isExpired: boolean;
+    gameId: string
+    reconnectionDeadline: Date
+    isExpired: boolean
 }
 
-const PENDING_GAME = "pending_game";
-const USER_TIMEOUT = "user_timeout";
-const TIMEOUT_IN_SECONDS = 60 * 5; // 5 minutes
+const PENDING_GAME = "pending_game"
+const USER_TIMEOUT = "user_timeout"
+const TIMEOUT_IN_SECONDS = 60 * 5 // 5 minutes
 
-export async function isPlayerTimeout(presence: Presence, playerId: string): Promise<boolean> {
-    const timeoutDateStr = await presence.hget(playerId, USER_TIMEOUT);
+export async function isPlayerTimeout(
+    presence: Presence,
+    playerId: string
+): Promise<boolean> {
+    const timeoutDateStr = await presence.hget(playerId, USER_TIMEOUT)
     if (timeoutDateStr) {
         const timeout = new Date(timeoutDateStr).getTime()
         //logger.debug(`Checking timeout for player ${playerId}: ${timeoutDateStr}`);
         return isValidDate(timeout) && Date.now() < timeout
     }
-    return false;
+    return false
 }
 
-export async function givePlayerTimeout(presence: Presence, playerId: string): Promise<void> {
+export async function givePlayerTimeout(
+    presence: Presence,
+    playerId: string
+): Promise<boolean> {
     //logger.debug(`Setting timeout for player ${playerId}: ${new Date(Date.now() + 1000 * TIMEOUT_IN_SECONDS).toISOString()}`);
     return presence.hset(
         playerId,
@@ -32,34 +38,53 @@ export async function givePlayerTimeout(presence: Presence, playerId: string): P
     )
 }
 
-export async function getPendingGame(presence: Presence, playerId: string): Promise<PendingGame | null> {
-    const pendingGame = await presence.hget(playerId, PENDING_GAME);
+export async function getPendingGame(
+    presence: Presence,
+    playerId: string
+): Promise<PendingGame | null> {
+    const pendingGame = await presence.hget(playerId, PENDING_GAME)
     if (pendingGame) {
-        const [pendingGameId, reconnectionDeadlineStr] = pendingGame.split(",");
-        const reconnectionDeadline = new Date(reconnectionDeadlineStr);
+        const [pendingGameId, reconnectionDeadlineStr] = pendingGame.split(",")
+        const reconnectionDeadline = new Date(reconnectionDeadlineStr)
         // check if date is valid
         if (!isValidDate(reconnectionDeadline)) {
             //logger.error("Invalid reconnection deadline date", pendingGameId, reconnectionDeadlineStr);
-            return null;
+            return null
         }
 
         //logger.debug(`Pending game found for player ${playerId}: ${pendingGameId}, deadline: ${reconnectionDeadline.toISOString()}`);
         return {
             gameId: pendingGameId,
             reconnectionDeadline,
-            isExpired: !isValidDate(reconnectionDeadline) || reconnectionDeadline.getTime() > Date.now()
-        };
+            isExpired:
+                !isValidDate(reconnectionDeadline) ||
+                reconnectionDeadline.getTime() > Date.now()
+        }
     }
     //logger.debug(`No pending game found for player ${playerId}`);
-    return null;
+    return null
 }
 
-export async function setPendingGame(presence: Presence, playerId: string, gameId: string): Promise<void> {
+export async function setPendingGame(
+    presence: Presence,
+    playerId: string,
+    gameId: string
+): Promise<boolean> {
     //logger.debug(`Setting pending game for player ${playerId}: ${gameId}, deadline: ${new Date(Date.now() + 1000 * ALLOWED_GAME_RECONNECTION_TIME).toISOString()}`);
-    return presence.hset(playerId, PENDING_GAME, [gameId, new Date(Date.now() + 1000 * ALLOWED_GAME_RECONNECTION_TIME).toISOString()].join(","));
+    return presence.hset(
+        playerId,
+        PENDING_GAME,
+        [
+            gameId,
+            new Date(Date.now() + 1000 * ALLOWED_GAME_RECONNECTION_TIME).toISOString()
+        ].join(",")
+    )
 }
 
-export async function clearPendingGame(presence: Presence, playerId: string): Promise<boolean> {
+export async function clearPendingGame(
+    presence: Presence,
+    playerId: string
+): Promise<boolean> {
     //logger.debug(`Clearing pending game for player ${playerId}`);
-    return presence.hdel(playerId, PENDING_GAME);
+    return presence.hdel(playerId, PENDING_GAME)
 }
