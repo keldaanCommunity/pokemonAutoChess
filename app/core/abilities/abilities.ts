64a2a527b3ad958d2a3657a5808ea34122de8d22
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-29 15:24:16 +0200

    Prod hotfixes (#3204)
    * fix getClosestAvailablePlaceOnBoardToPokemon
    
    * remove cotton ball effect after being applied
    
    * prevent cheating in pokeguessr
    
    * fixed meditate ability anim
    
    * revert colyseus schema to 3.0.26
    
    * fix mr conflict for boom burst ability
    
    * fix retargeting exception when confusion ends
    
    * fix VersionError for OpenBoosterCommand

--- app/core/abilities/abilities.ts
+++ app/core/abilities/abilities.ts
@@ -12571,29 +12571,17 @@ export class BoomBurstStrategy extends AbilityStrategy {
   ) {
     // Deal 60 special damage to all adjacent units including allies
     super.process(pokemon, board, target, crit)
-    const damage = [20, 40, 80][pokemon.stars - 1] ?? 80
-    const farthestCoordinate =
-      board.getFarthestTargetCoordinateAvailablePlace(pokemon)
-    if (farthestCoordinate) {
-      const cells = board.getCellsBetween(
-        pokemon.positionX,
-        pokemon.positionY,
-        farthestCoordinate.x,
-        farthestCoordinate.y
-      )
-      cells.forEach((cell) => {
-        if (cell.value && cell.value.team != pokemon.team) {
-          cell.value.handleSpecialDamage(
-            damage,
-            board,
-            AttackType.SPECIAL,
-            pokemon,
-            crit
-          )
-        }
-      })
-      pokemon.moveTo(farthestCoordinate.x, farthestCoordinate.y, board)
-    }
+    const damage = 60
+    board.getAdjacentCells(
+      pokemon.positionX,
+      pokemon.positionY,
+      false
+    ).forEach((cell) => {
+      if (cell.value) {
+        cell.value.handleSpecialDamage(damage, board, AttackType.SPECIAL, pokemon, crit)
+        cell.value.status.triggerFlinch(4000, cell.value, pokemon)
+      }
+    })
   }
 }
 
