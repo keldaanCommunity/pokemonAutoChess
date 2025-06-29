ccb10cbef42b27b75edbdf8879facde42270f71e
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-29 17:34:55 +0200

    another arceus dragon bugfix

--- app/models/colyseus-models/synergies.ts
+++ app/models/colyseus-models/synergies.ts
@@ -101,42 +101,6 @@ export function computeSynergies(
     })
   })
 
-  // add dynamic synergies (Arceus & Kecleon)
-  board.forEach((pkm: IPokemon) => {
-    if (
-      pkm.positionY !== 0 &&
-      (pkm.passive === Passive.PROTEAN2 || pkm.passive === Passive.PROTEAN3)
-    ) {
-      const n = pkm.passive === Passive.PROTEAN3 ? 3 : 2
-      const synergiesSorted = [...synergies.keys()].sort((a, b) => {
-        if (a === Synergy.DRAGON) return -1
-        if (b === Synergy.DRAGON) return 1
-        return +synergies.get(b)! - +synergies.get(a)!
-      })
-      const family = PkmFamily[pkm.name]
-      if (!dynamicTypesPerFamily.has(family))
-        dynamicTypesPerFamily.set(family, new Set())
-      const types: Set<Synergy> = dynamicTypesPerFamily.get(family)!
-
-      for (let i = 0; i < n; i++) {
-        const type = synergiesSorted[i]
-        if (type && !pkm.types.has(type) && synergies.get(type)! > 0) {
-          pkm.types.add(type)
-          types.add(type)
-          if (type === Synergy.DRAGON && (i + 1) in synergiesSorted) {
-            dragonDoubleTypes.set(family, new Set([synergiesSorted[i + 1]]))
-          }
-        }
-      }
-    }
-  })
-
-  dynamicTypesPerFamily.forEach((types) => {
-    types.forEach((type, i) => {
-      synergies.set(type, (synergies.get(type) ?? 0) + 1)
-    })
-  })
-
   // apply dragon double synergies
   if (
     (synergies.get(Synergy.DRAGON) ?? 0) >= SynergyTriggers[Synergy.DRAGON][0]
@@ -147,6 +111,39 @@ export function computeSynergies(
       })
     })
   }
+
+  // add dynamic synergies (Arceus & Kecleon)
+  board.forEach((pkm: IPokemon) => {
+    if (
+      pkm.positionY !== 0 &&
+      (pkm.passive === Passive.PROTEAN2 || pkm.passive === Passive.PROTEAN3)
+    ) {
+      const nbDynamicSynergies = pkm.passive === Passive.PROTEAN3 ? 3 : 2
+      const synergiesSorted = [...synergies.keys()].sort((a, b) => +synergies.get(b)! - +synergies.get(a)!)
+
+      if (synergiesSorted.slice(0, nbDynamicSynergies).includes(Synergy.DRAGON)) {
+        // if dragon is in the top synergies, we need to ensure it is the first one
+        const dragonIndex = synergiesSorted.indexOf(Synergy.DRAGON)
+        if (dragonIndex > 0) {
+          synergiesSorted.splice(dragonIndex, 1)
+          synergiesSorted.unshift(Synergy.DRAGON)
+        }
+      }
+
+      for (let i = 0; i < nbDynamicSynergies; i++) {
+        const type = synergiesSorted[i]
+        if (type && !pkm.types.has(type) && synergies.get(type)! > 0) {
+          pkm.types.add(type)
+          synergies.set(type, (synergies.get(type) ?? 0) + 1)
+          //apply dragon double synergies just for Arceus & Kecleon if Dragon
+          if (type === Synergy.DRAGON && synergies.get(Synergy.DRAGON)! >= SynergyTriggers[Synergy.DRAGON][0]) {
+            const doubledType = synergiesSorted[1]
+            synergies.set(doubledType, (synergies.get(doubledType) ?? 0) + 1)
+          }
+        }
+      }
+    }
+  })
 
   return synergies
 }
