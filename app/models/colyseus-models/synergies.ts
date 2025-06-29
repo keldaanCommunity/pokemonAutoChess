b8883cd3077cfde4bdfcbcfa1f6b98c019021202
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-29 20:08:31 +0200

    Another arceus dragon bugfix (#3207)
    * another arceus dragon bugfix
    
    * fix dragon double type application if arceus reached dragon 3 with dynamic synergy
    
    * fix dragon double type application if arceus reached dragon 3 with dynamic synergy

--- app/models/colyseus-models/synergies.ts
+++ app/models/colyseus-models/synergies.ts
@@ -68,7 +68,6 @@ export function computeSynergies(
   })
 
   const typesPerFamily = new Map<Pkm, Set<Synergy>>()
-  const dynamicTypesPerFamily = new Map<Pkm, Set<Synergy>>()
   const dragonDoubleTypes = new Map<Pkm, Set<Synergy>>()
 
   board.forEach((pkm: IPokemon) => {
@@ -101,52 +100,58 @@ export function computeSynergies(
     })
   })
 
+  function applyDragonDoubleTypes() {
+    if (
+      (synergies.get(Synergy.DRAGON) ?? 0) >= SynergyTriggers[Synergy.DRAGON][0]
+    ) {
+      dragonDoubleTypes.forEach((types) => {
+        types.forEach((type, i) => {
+          synergies.set(type, (synergies.get(type) ?? 0) + 1)
+        })
+      })
+    }
+  }
+
+  applyDragonDoubleTypes()
+
   // add dynamic synergies (Arceus & Kecleon)
   board.forEach((pkm: IPokemon) => {
     if (
       pkm.positionY !== 0 &&
       (pkm.passive === Passive.PROTEAN2 || pkm.passive === Passive.PROTEAN3)
     ) {
-      const n = pkm.passive === Passive.PROTEAN3 ? 3 : 2
-      const synergiesSorted = [...synergies.keys()].sort((a, b) => {
-        if (a === Synergy.DRAGON) return -1
-        if (b === Synergy.DRAGON) return 1
-        return +synergies.get(b)! - +synergies.get(a)!
-      })
-      const family = PkmFamily[pkm.name]
-      if (!dynamicTypesPerFamily.has(family))
-        dynamicTypesPerFamily.set(family, new Set())
-      const types: Set<Synergy> = dynamicTypesPerFamily.get(family)!
+      const nbDynamicSynergies = pkm.passive === Passive.PROTEAN3 ? 3 : 2
+      const synergiesSorted = [...synergies.keys()].sort((a, b) => +synergies.get(b)! - +synergies.get(a)!)
 
-      for (let i = 0; i < n; i++) {
+      if (synergiesSorted.slice(0, nbDynamicSynergies).includes(Synergy.DRAGON)) {
+        // if dragon is in the top synergies, we need to ensure it is the first one
+        const dragonIndex = synergiesSorted.indexOf(Synergy.DRAGON)
+        if (dragonIndex > 0) {
+          synergiesSorted.splice(dragonIndex, 1)
+          synergiesSorted.unshift(Synergy.DRAGON)
+        }
+      }
+
+      for (let i = 0; i < nbDynamicSynergies; i++) {
         const type = synergiesSorted[i]
         if (type && !pkm.types.has(type) && synergies.get(type)! > 0) {
           pkm.types.add(type)
-          types.add(type)
-          if (type === Synergy.DRAGON && (i + 1) in synergiesSorted) {
-            dragonDoubleTypes.set(family, new Set([synergiesSorted[i + 1]]))
+          synergies.set(type, (synergies.get(type) ?? 0) + 1)
+          //apply dragon double synergies just for Arceus & Kecleon if Dragon
+          if (type === Synergy.DRAGON) {
+            if (synergies.get(Synergy.DRAGON)! === SynergyTriggers[Synergy.DRAGON][0]) {
+              // Arceus/Kecleon just activated Dragon 3, so we need to apply the double synergies to all pokemons
+              applyDragonDoubleTypes()
+            } else if (synergies.get(Synergy.DRAGON)! > SynergyTriggers[Synergy.DRAGON][0]) {
+              // Dragon 3 was already activated, so we just need to double the synergy of Arceus/Kecleon
+              const doubledType = synergiesSorted[1]
+              synergies.set(doubledType, (synergies.get(doubledType) ?? 0) + 1)
+            }
           }
         }
       }
     }
   })
-
-  dynamicTypesPerFamily.forEach((types) => {
-    types.forEach((type, i) => {
-      synergies.set(type, (synergies.get(type) ?? 0) + 1)
-    })
-  })
-
-  // apply dragon double synergies
-  if (
-    (synergies.get(Synergy.DRAGON) ?? 0) >= SynergyTriggers[Synergy.DRAGON][0]
-  ) {
-    dragonDoubleTypes.forEach((types) => {
-      types.forEach((type, i) => {
-        synergies.set(type, (synergies.get(type) ?? 0) + 1)
-      })
-    })
-  }
 
   return synergies
 }
