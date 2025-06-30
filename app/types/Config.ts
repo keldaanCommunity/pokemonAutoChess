576b87e1f05f7c8c5fad0bfe7fcf03fa6e3063b2
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-06-30 23:29:36 +0200

    add missing legendaries

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -364,7 +364,10 @@ export const LegendaryPool = new Array<PkmProposition>(
   Pkm.PECHARUNT,
   Pkm.ROARING_MOON,
   Pkm.ZACIAN,
-  Pkm.IRON_VALIANT
+  Pkm.IRON_VALIANT,
+  Pkm.OKIDOGI,
+  Pkm.MUNKIDORI,
+  Pkm.FEZANDIPITI
 )
 
 export const NB_UNIQUE_PROPOSITIONS = 6
