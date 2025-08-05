81e745a3443a712be3731c5c0ae0ec2639061e33
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-08-05 07:35:52 +0200

    fix max interest (#3270)

--- app/public/src/pages/component/game/game-money-info.tsx
+++ app/public/src/pages/component/game/game-money-info.tsx
@@ -22,7 +22,7 @@ export function GameMoneyInfo() {
         </Tooltip>
         <Money
           value={currentPlayer.money}
-          className={cc({ "is-max": currentPlayer.money >= maxInterest })}
+          className={cc({ "is-max": currentPlayer.money >= maxInterest * 10 })}
         />
       </div>
     </div>
