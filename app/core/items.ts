ee1fac9b2320a0a26b6cca365f59a676b4f8ef43
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-08-13 07:49:40 +0200

    nerf green orb (#3288)
    * nerf green orb
    
    * Update app/public/dist/client/locales/zh/translation.json
    
    Co-authored-by: Benzene114 <81860776+Benzene114@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Benzene114 <81860776+Benzene114@users.noreply.github.com>

--- app/core/items.ts
+++ app/core/items.ts
@@ -48,7 +48,7 @@ export const ItemStats: { [item in Item]?: { [stat in Stat]?: number } } = {
   [Item.BLUE_ORB]: { [Stat.PP]: 15, [Stat.SPEED]: 10 },
   [Item.SCOPE_LENS]: { [Stat.PP]: 15, [Stat.CRIT_CHANCE]: 25 },
   [Item.STAR_DUST]: { [Stat.PP]: 15, [Stat.SHIELD]: 15 },
-  [Item.GREEN_ORB]: { [Stat.PP]: 15, [Stat.SPE_DEF]: 3 },
+  [Item.GREEN_ORB]: { [Stat.SPE_DEF]: 3 },
   [Item.DEEP_SEA_TOOTH]: { [Stat.PP]: 15, [Stat.ATK]: 7 },
   [Item.SMOKE_BALL]: { [Stat.CRIT_CHANCE]: 10 },
   [Item.XRAY_VISION]: { [Stat.SPEED]: 50 },
--- app/public/dist/client/changelog/patch-6.5.md
+++ app/public/dist/client/changelog/patch-6.5.md
@@ -8,6 +8,8 @@
 
 # Changes to Items
 
+- Nerf Green Orb: removed flat part of the healing, removed base 15 PP
+
 # Gameplay
 
 - Ranked matches created by Ultra ball rank and above are now open with a min elo requirement of Super ball rank instead of Pokeball rank. The previous overlap was too large and ended up counter-productive.
