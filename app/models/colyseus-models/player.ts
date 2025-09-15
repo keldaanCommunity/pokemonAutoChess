94ce6ba0ec5282a812668f0d8e785af28d347a04
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-09-15 18:50:07 +0200

    remove useless line

--- app/models/colyseus-models/player.ts
+++ app/models/colyseus-models/player.ts
@@ -663,7 +663,6 @@ export default class Player extends Schema implements IPlayer {
     )
     // special case for burmy line because of the exclusive convergent evolution rule
     if (finals.has(Pkm.BURMY_PLANT)) {
-      finals.add(Pkm.BURMY_PLANT)
       finals.add(Pkm.BURMY_TRASH)
       finals.add(Pkm.BURMY_SANDY)
     }
