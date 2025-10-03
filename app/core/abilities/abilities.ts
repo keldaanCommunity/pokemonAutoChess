58bc619356ca78c9621dac29df61b948b03044ab
Shadrach Jensen <jensen.shadrach@gmail.com>
2025-10-02 23:50:50 -0600

    Fix Cut Damage
    Despite a buff back in 5.10, the cut damage value was never changed only the description was. Buff damage to 40% instead of 30%

--- app/core/abilities/abilities.ts
+++ app/core/abilities/abilities.ts
@@ -11111,8 +11111,8 @@ export class CutStrategy extends AbilityStrategy {
     crit: boolean
   ) {
     super.process(pokemon, board, target, crit)
-    //Deal [30,SP]% of target max HP as special damage. Inflicts WOUND for 5 seconds
-    const damage = 0.3 * target.hp
+    //Deal [40,SP]% of target max HP as special damage. Inflicts WOUND for 5 seconds
+    const damage = 0.4 * target.hp
     target.handleSpecialDamage(damage, board, AttackType.SPECIAL, pokemon, crit)
     target.status.triggerWound(5000, target, pokemon)
   }
