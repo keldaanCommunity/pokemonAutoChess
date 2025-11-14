ab79d51c6d5d6d871e6e2fc0e93ea74e5fe21585
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-11-15 00:47:03 +0100

    fix username regex

--- app/config/server/rules.ts
+++ app/config/server/rules.ts
@@ -1,6 +1,6 @@
 export const MAX_USER_NAME_LENGTH = 24
 export const MIN_USER_NAME_LENGTH = 3
 export const USERNAME_REGEXP = new RegExp(
-  `^(\\p{Letter}|[0-9]|\\.|\\-|_){${MIN_USER_NAME_LENGTH},${MAX_USER_NAME_LENGTH}}$`,
+  `^[\\p{L}0-9._-]{${MIN_USER_NAME_LENGTH},${MAX_USER_NAME_LENGTH}}$`,
   "u"
 )
