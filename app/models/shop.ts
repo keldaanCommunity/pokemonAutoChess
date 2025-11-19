90870d16a0e1f7744c9502aadbc684798f2d46e5
keldaan-ag <keldaan.ag@gmail.com>
2025-11-19 07:26:40 +0100

    shop

--- app/models/shop.ts
+++ app/models/shop.ts
@@ -180,9 +180,7 @@ export function getBuyPrice(
   return price
 }
 
-const CommonShop = getRegularsTier1(
-  PRECOMPUTED_POKEMONS_PER_RARITY.COMMON
-).concat(Pkm.ORTHWORM)
+const CommonShop = getRegularsTier1(PRECOMPUTED_POKEMONS_PER_RARITY.COMMON)
 const UncommonShop = getRegularsTier1(PRECOMPUTED_POKEMONS_PER_RARITY.UNCOMMON)
 const RareShop = getRegularsTier1(PRECOMPUTED_POKEMONS_PER_RARITY.RARE)
 const EpicShop = getRegularsTier1(PRECOMPUTED_POKEMONS_PER_RARITY.EPIC)
