4efbef72b78d66653c4df629b05fe24bd5ac5c90
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2025-11-22 02:09:58 +0100

    fix gear grind

--- app/core/abilities/abilities.ts
+++ app/core/abilities/abilities.ts
@@ -15163,6 +15163,7 @@ export class GearGrindStrategy extends AbilityStrategy {
     crit: boolean
   ) {
     // Launches two gears at the target, each dealing [50,100,200,SP]% of SPEED as SPECIAL
+    super.process(pokemon, board, target, crit)
     const speedFactor = [0.5, 1, 2][pokemon.stars - 1] ?? 2
     const damage = Math.round(pokemon.speed * speedFactor)
     for (let i = 0; i < 2; i++) {
