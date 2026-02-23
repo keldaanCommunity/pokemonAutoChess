d36f4a409939aabbb5876bce96c4b5c867aef850
keldaan-ag <keldaan.ag@gmail.com>
2026-02-23 15:23:59 +0100

    sprite generation

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v6.8.0.768"
+const CACHE_NAME = "CACHE v6.8.0.769"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
