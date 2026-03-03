a3b7512ec3234f471421852866dcbb12b5f5a981
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2026-03-03 19:46:47 +0100

    simplify finizen passive code

--- app/core/effects/passives.ts
+++ app/core/effects/passives.ts
@@ -1389,40 +1389,12 @@ export const PassiveEffects: Partial<
   [Passive.FINIZEN]: [
     new OnSimulationStartEffect(({ simulation, entity }) => {
       if (entity.name === Pkm.PALAFIN_HERO) return
-      let alliesKo = 0
-      let alliesNb = 0
+      let nbAlliesKo = 0
+      let nbAllies = 0
       let transformed = false
-      simulation.board.forEach((x, y, pkm) => {
-        if (pkm && pkm.team === entity.team && pkm.id !== entity.id) {
-          alliesNb++
-          pkm.effectsSet.add(
-            new OnDeathEffect(() => {
-              alliesKo++
-              if (!transformed && (alliesKo >= 5 || alliesKo >= alliesNb)) {
-                transformed = true
-                entity.index = PkmIndex[Pkm.PALAFIN_HERO]
-                entity.name = Pkm.PALAFIN_HERO
-                // TODO: update stats to match the hero form
-                entity.addAttack(18, entity, 0, false)
-                entity.addSpeed(16, entity, 0, false)
-                entity.addDefense(5, entity, 0, false)
-                entity.addSpecialDefense(5, entity, 0, false)
-                entity.hp = entity.maxHP
-                if (entity.player) {
-                  entity.player.pokemonsPlayed.add(Pkm.PALAFIN_HERO)
-                  entity.player.transformPokemon(
-                    entity.refToBoardPokemon as Pokemon,
-                    Pkm.PALAFIN
-                  )
-                }
-              }
-            })
-          )
-        }
-      })
 
-      // edge case no allies
-      if (alliesNb === 0) {
+      const transformToHero = () => {
+        transformed = true
         entity.index = PkmIndex[Pkm.PALAFIN_HERO]
         entity.name = Pkm.PALAFIN_HERO
         entity.addAttack(18, entity, 0, false)
@@ -1432,10 +1404,31 @@ export const PassiveEffects: Partial<
         entity.hp = entity.maxHP
         if (entity.player) {
           entity.player.pokemonsPlayed.add(Pkm.PALAFIN_HERO)
-          entity.player.transformPokemon(entity.refToBoardPokemon as Pokemon, Pkm.PALAFIN)
+          entity.player.transformPokemon(
+            entity.refToBoardPokemon as Pokemon,
+            Pkm.PALAFIN
+          )
         }
       }
 
+      const transformToHeroOnDeathEffect = new OnDeathEffect(() => {
+        nbAlliesKo++
+        if (!transformed && (nbAlliesKo >= 5 || nbAlliesKo >= nbAllies)) {
+          transformToHero()
+        }
+      })
+
+      simulation.board.forEach((x, y, pkm) => {
+        if (pkm && pkm.team === entity.team && pkm.id !== entity.id) {
+          nbAllies++
+          pkm.effectsSet.add(transformToHeroOnDeathEffect)
+        }
+      })
+
+      // edge case no allies: transform immediately
+      if (nbAllies === 0) {
+        transformToHero()
+      }
     })
   ]
 }
