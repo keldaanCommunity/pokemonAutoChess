f1529c5e39061db0165a60f48a97eee62aef7e49
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2026-03-29 15:40:01 +0200

    remove temporary changes made to test expeditions event

--- app/rooms/game-room.ts
+++ app/rooms/game-room.ts
@@ -999,10 +999,9 @@ export default class GameRoom extends Room<{ state: GameState }> {
       })
 
       if (
-        getCurrentGameEvent() === GameEvent.EXPEDITIONS /*&&
+        getCurrentGameEvent() === GameEvent.EXPEDITIONS &&
         eligibleToXP &&
-        this.state.gameMode !== GameMode.CUSTOM_LOBBY */
-        // TODO: remove this condition after testing, we want to allow expeditions points gain in custom lobby for testing purposes
+        this.state.gameMode !== GameMode.CUSTOM_LOBBY
       ) {
         const hasCompletedExpeditions = updatePlayerExpeditionsAfterGame(
           player,
