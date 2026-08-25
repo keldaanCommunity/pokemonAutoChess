0dfce6a75cb2d9b520f2226d9ea5e2dc35894f78
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2026-08-25 19:37:23 +0200

    fix long wand range reset after locked

--- app/models/colyseus-models/status.ts
+++ app/models/colyseus-models/status.ts
@@ -1185,7 +1185,8 @@ export default class Status extends Schema implements IStatus {
       if (
         pokemon.player &&
         pokemon.player.items.includes(Item.LONG_WAND) &&
-        pokemon.types.has(Synergy.FAIRY)
+        pokemon.types.has(Synergy.FAIRY) &&
+        pokemon.baseRange > 1
       ) {
         range += 1
       }
